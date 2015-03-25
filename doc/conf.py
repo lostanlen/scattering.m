4fdcea1900414df4c8a38713a579d8263f872064
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-03-25 17:18:01 +0100

    RTD now compiles. Re-enable MathJax

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ extensions = [
  #   'sphinx.ext.intersphinx',
 #    'sphinx.ext.todo',
 #    'sphinx.ext.coverage',
-#    'sphinx.ext.mathjax',
+    'sphinx.ext.mathjax',
 #    'sphinxcontrib.matlab',
 ]
 
