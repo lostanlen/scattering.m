f719d7d060bf6899f66bb18fa2c4a3ac26d9cc23
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-03-30 18:17:24 +0200

    Initial commit for sc_substract

---
+++ lib/backward/sc_substract.m
@@ -0,0 +1,13 @@
+function dS = sc_substract(S_minuend,S_subtrahend)
+%% Initialization
+nLayers = length(S_minuend);
+dS = cell(1,nLayers);
+
+%% Loop over layers
+for layer_index = 0:nLayers-1
+    %% Pairwise substraction of minuend and subtrahend layers
+    layer_S_minuend = S_minuend{1+layer_index};
+    layer_S_subtrahend = S_subtrahend{1+layer_index};
+    dS{1+layer_index} = substract_layers(layer_S_minuend,layer_S_subtrahend);
+end
+end
