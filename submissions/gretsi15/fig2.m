e410c84313a65ad29366cc7550cfbcf248b07754
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-01 13:13:43 +0200

    Update GRETSI figures with new function names

--- submissions/gretsi15/fig2.m
+++ submissions/gretsi15/fig2.m
@@ -9,21 +9,21 @@ opts{2}.time.handle = @gammatone_1d;
 opts{2}.time.max_scale = Inf;
 opts{2}.time.max_Q = 2;
 opts{2}.time.U_log2_oversampling = 2;
+opts{2}.time.gamma_bounds = [8 Inf];
 
 % Options for scattering along chromas
 opts{2}.gamma.invariance = 'bypassed';
 opts{2}.gamma.U_log2_oversampling = Inf;
 
 % Options for scattering along octaves
-opts{2}.j.handle = @poisson_1d;
+opts{2}.j.handle = @RLC_1d;
 opts{2}.j.invariance = 'bypassed';
-opts{2}.j.mother_xi = 0.33;
-opts{2}.j.decay_factor = 1/4;
+opts{2}.j.mother_xi = 0.5;
 
 % Build scattering "architectures", i.e. filter banks and nonlinearities
-archs = setup(opts);
+archs = sc_setup(opts);
 
-% We start by computing an empty scalogram
+%% We start by computing an empty scalogram
 signal = zeros(N,1);
 U{1+0} = initialize_U(signal);
 Y{1} = U_to_Y(U{1+0},archs{1});
@@ -40,16 +40,23 @@ hot_colormap = hot();
 reverse_hot_colormap = hot_colormap(end:-1:1,:);
 
 %% Figure 2a
-spiral_scattergram = Y{2}{1+3}{1,1,1}.data{5}{1,1};
+time_scale = 10;
+chroma_scale = 1;
+octave_scale = 2;
+chroma_sign = 2;
+octave_sign = 1;
+
+spiral_scattergram = ...
+    Y{2}{1+3}{1,1,1}.data{time_scale}{chroma_scale,octave_scale};
 sizes = size(spiral_scattergram);
 scattergram_sizes = [sizes(1),sizes(2)*sizes(3),sizes(4),sizes(5)];
 spiral_scattergram = reshape(spiral_scattergram,scattergram_sizes);
-gamma_range = 1:70;
+gamma_range = 1:(min(Inf,size(spiral_scattergram,2)));
 spiral_scattergram = real(spiral_scattergram(:,gamma_range,:,:));
-spiral_scattergram(abs(spiral_scattergram)<1e-5) = 0;
+spiral_scattergram(abs(spiral_scattergram)<1e-4) = 0;
 normalizer = max(abs(spiral_scattergram(:)));
 spiral_scattergram = 32 + 32 * spiral_scattergram/normalizer;
 colormap(reverse_hot_colormap);
-image(spiral_scattergram(:,:,2,1)');
+image(spiral_scattergram(:,:,chroma_sign,octave_sign)');
 axis off;
-export_fig raw_fig2a.png -transparent
+%export_fig raw_fig2a.png -transparent
