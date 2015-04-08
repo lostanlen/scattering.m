c38f00b50534c596f24547f775a6a9ba5717c788
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-08 18:30:28 +0200

    Bug fix ranges{1:end-1} -> ranges(1:end-1) in get_ranges_node

--- lib/hierarchy/get_ranges_node.m
+++ lib/hierarchy/get_ranges_node.m
@@ -1,3 +1,3 @@
 function ranges_node = get_ranges_node(ranges,node)
-ranges_node = cellfun(@(x) x{node},ranges{1:end-1},'UniformOutput',false);
+ranges_node = cellfun(@(x) x{node},ranges(1:end-1),'UniformOutput',false);
 end
