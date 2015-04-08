0b4f99cb4cd4ff1270818eb89cd5703ce9a9d088
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-08 18:29:56 +0200

    Introduce special case for last level in set_ranges_node

--- lib/hierarchy/set_ranges_node.m
+++ lib/hierarchy/set_ranges_node.m
@@ -1,5 +1,7 @@
 function ranges = set_ranges_node(ranges,ranges_node,node)
-for level = 0:(length(ranges_node)-1)
-    ranges{end-level}{node} = ranges_node{end-level}{node};
+nLevels = length(ranges);
+ranges{end-1}{node} = ranges_node{end};
+for level = 2:(nLevels-1)
+    ranges{end-level}{node} = ranges_node{end-level+1};
 end
 end
