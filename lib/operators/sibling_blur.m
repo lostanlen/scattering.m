39447cdaf963ff45d6f660b6046c22a559ec5282
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-08 14:51:41 +0200

    Fix samplings and n-way statements in sibling_blur

--- lib/operators/sibling_blur.m
+++ lib/operators/sibling_blur.m
@@ -9,61 +9,55 @@ if level_counter>0
         % Recursive call
         ranges_node = get_ranges_node(ranges,node);
         [data{node},ranges_node] = ...
-           sibling_blur(data_ft{node},bank,ranges_node,sibling);
+            sibling_blur(data_ft{node},bank,ranges_node,sibling);
         ranges = set_ranges_node(ranges,ranges_node,node);
     end
     if length(input_sizes)>1
         data = reshape(data,input_sizes);
     end
     return;
-elseif level_counter==0 && ~isnumeric(data_ft)
-    nCousins = prod(input_sizes);
 end
 
 %% Selection of signal-adapted support for the filter bank
-signal_range = get_signal_range(ranges{1+0},bank.behavior.subscript);
+bank_behavior = bank.behavior;
+subscripts = bank_behavior.subscripts;
+signal_range = get_signal_range(ranges{1+0},subscripts);
 signal_log2_support = nextpow2(min(signal_range(end,:)-signal_range(1,:)+1));
 support_index = log2(bank.spec.size) - signal_log2_support + 1;
 phi = bank.phi{support_index};
 
 %% Definition of resampling factors
-sibling_log2_samplings = [sibling.metas.log2_sampling];
+sibling_gammas = collect_range(ranges{1+1}(:,sibling.subscripts));
+log2_resolutions = [sibling.metas(sibling_gammas).log2_resolution];
+sibling_log2_oversampling = sibling.behavior.U.log2_oversampling;
+log2_factor = sibling.behavior.sibling_mask_factor;
+sibling_log2_samplings = ...
+    min(log2_resolutions+sibling_log2_oversampling,log2_factor);
 critical_log2_sampling = 1 - bank.spec.J;
-log2_oversampling = bank.behavior.S.log2_oversampling;
-log2_sampling = log2_oversampling + critical_log2_sampling;
+log2_sampling = sibling_log2_oversampling + critical_log2_sampling;
 critical_log2_resamplings = critical_log2_sampling - sibling_log2_samplings;
+log2_oversampling = bank_behavior.S.log2_oversampling;
 log2_resamplings = log2_oversampling + critical_log2_resamplings;
 
 %% Update of ranges at zeroth level (tensor level)
 ranges{1+0} = update_range_step(ranges{1+0},log2_sampling,subscripts);
 
-%% Update of ranges at first level (gamma level)
-
-
 %% If data is multidimensional, reshape it to a matrix
 % The rows are the sibling gammas (e.g. "gamma_1" or "gamma_2")
 % The columns are the "cousins" (e.g. gammas across other variables)
 % This happens in all multi-variable architectures
-data_ft_sizes = drop_trailing(size(data_ft));
-nData_dimensions = length(data_ft_sizes);
-if nData_dimensions>1
-    cousin_subscripts = find((1:nData_dimensions)~=sibling.subscript);
-    cousin_sizes = data_ft_sizes(cousin_subscripts);
-    nCousins = prod(cousin_sizes);
-    if sibling.subscript>1
-        permuted_subscripts = [sibling.subscript,cousin_subscripts];
-        data_ft = permute(data_ft,permuted_subscripts);
-    end
-    nSibling_gammas = data_ft_sizes(sibling.subscript);
-    data_ft = reshape(data_ft,[nSibling_gammas,nCousins]);
-else
-    nCousins = 1;
+nData_dimensions = length(input_sizes);
+cousin_subscripts = find((1:nData_dimensions)~=sibling.subscripts);
+nCousins = prod(cousin_subscripts);
+if sibling.subscripts>1
+    permuted_subscripts = [sibling.subscripts,cousin_subscripts];
+    data_ft = permute(data_ft,permuted_subscripts);
 end
+nSibling_gammas = input_sizes(sibling.subscripts);
+data_ft = reshape(data_ft,[nSibling_gammas,nCousins]);
 
 %% Initialization of output data
 data = cell([nSibling_gammas,nCousins]);
-nSibling_gammas = length(log2_resamplings);
-bank_behavior = bank.behavior;
 
 %% Blurring implementation
 % This loop can be parallelized
@@ -79,7 +73,7 @@ for cousin_index = 1:nCousins
 end
 
 %% Reshaping of linear cell array back to its original multidimensional format
-if nCousins>1
+if nData_dimensions>1
     data = reshape(data,[data_ft_sizes,1]);
 end
 end
