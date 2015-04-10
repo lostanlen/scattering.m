15c382ddc9ef863240fbc9a94f5b7a18f5a0716d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-10 15:42:15 +0200

    Refactor nephew_blur in the vein of new nephew_scatter

--- lib/operators/nephew_blur.m
+++ lib/operators/nephew_blur.m
@@ -1,20 +1,26 @@
-function data = ...
-    nephew_blur(data_ft,bank,sibling,uncle,uncle_level_counter)
-if uncle_level_counter>0
-    % TODO: map explicitly
+function [data,ranges] = nephew_blur(data_ft,bank,ranges,sibling,uncle)
+%% Deep map across levels
+level_counter = length(ranges) - 1 - uncle.level;
+input_sizes = drop_trailing(size(data_ft),1);
+if level_counter>0
+    data = cell([input_sizes,1]);
+    for node = 1:numel(data_ft)
+        node_ranges = get_ranges_node(ranges,node);
+        % Recursive call
+        [data{node},ranges_node] = ...
+            nephew_blur(data_ft{node},bank,node_ranges,sibling,uncle);
+        ranges = set_ranges_node(ranges,ranges_node,node);
+    end
     return;
 end
 
-%%
-% TODO: write generic code that finds level_counter
-level_counter = 0;
-data_ft_sizes = drop_trailing(size(data_ft),1);
+%% If data is multidimensional, reshape it to a matrix
 uncle_subscript = uncle.subscripts;
-nUncle_gammas = data_ft_sizes(uncle_subscript);
-nData_dimensions = length(data_ft_sizes);
+nUncle_gammas = input_sizes(uncle_subscript);
+nData_dimensions = length(input_sizes);
 if nData_dimensions>1
     cousin_subscripts = find((1:nData_dimensions)~=uncle_subscript);
-    cousin_sizes = data_ft_sizes(cousin_subscripts);
+    cousin_sizes = input_sizes(cousin_subscripts);
     nCousins = prod(cousin_sizes);
     if uncle.subscripts(1)>1
         permuted_subscripts = [uncle_subscript,cousin_subscripts];
@@ -26,37 +32,28 @@ else
 end
 data = cell(nUncle_gammas,nCousins);
 
-suffix_name = get_suffix(bank.behavior.key);
-switch suffix_name
-    case 'gamma'
-        log2_supports = ...
-            nextpow2(bank.spec.T/2 + [uncle.metas.max_sibling_gamma]);
-    case 'j'
-        log2_supports = ...
-            nextpow2(bank.spec.T/2 + [uncle.metas.max_sibling_j]);
+%% Fallback to firstborn_scatter and secondborn_scatter
+if nData_dimensions>1
+    error('nephew_blur for multiple variables at uncle level not ready yet');
 end
-support_indices = log2(bank.spec.size) - log2_supports + 1;
 
-%%
-% Caution: lowest values of uncle_index may bring empty banks if their
-% max_sibling_gammas is below the bank.metas.scale(1);
+input_ranges = ranges;
 for uncle_index = 1:nUncle_gammas
-    nephew_bank = bank;
-    nephew_bank.phi = bank.phi(support_indices(uncle_index));
+    ranges_node = get_ranges_node(input_ranges,uncle_index);
     if isempty(sibling)
-        nephew_bank = firstborn_blur_bank(nephew_bank);
-        if nData_dimensions==1
-            data{uncle_index} = ...
-                firstborn_blur(data_ft{uncle_index}, ...
-                nephew_bank,level_counter);
-        else
-            data{uncle_index} = ...
-                firstborn_blur(data_ft(uncle_index,:), ...
-                nephew_bank,level_counter);
-        end
+        [data{uncle_index},ranges_node] = ...
+            firstborn_blur(data_ft{uncle_index},bank,ranges_node);
+    elseif sibling.is_firstborn
+        [data{uncle_index},ranges_node] = ...
+            secondborn_blur(data_ft{uncle_index},bank,ranges_node,sibling);
     else
-        % This is needed e.g. for second-order blurring along gamma
-        error('sibling scattering not ready yet in nephew_blur')
+        [data{uncle_index},ranges_node] = ...
+            sibling_blur(data_ft{uncle_index},bank,ranges_node,sibling);
+    end
+    try
+    ranges = set_ranges_node(ranges,ranges_node,uncle_index);
+    catch err
+        disp(err);
     end
 end
 end
