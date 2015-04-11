beb03ba4989b92d17140821d23f78984815df81a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-11 16:01:44 +0200

    Speed up get_ranges_node with an explicit loop instead of cellfun

--- lib/hierarchy/get_ranges_node.m
+++ lib/hierarchy/get_ranges_node.m
@@ -1,3 +1,7 @@
 function ranges_node = get_ranges_node(ranges,node)
-ranges_node = cellfun(@(x) x{node},ranges(1:end-1),'UniformOutput',false);
+% This is faster than a cellfun
+ranges_node = cell(1,length(ranges)-1);
+for layer_index = 1:(length(ranges)-1)
+    ranges_node{layer_index} = ranges{layer_index}{node};
+end
 end
