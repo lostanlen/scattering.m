4da02b7b1b59615e24f5b3412d37d9725fa2b62f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-13 10:50:42 +0200

    Rename data_size -> input_size in prepare_firstborn_scatter_nospiral

--- lib/hierarchy/prepare_firstborn_scatter_nospiral.m
+++ lib/hierarchy/prepare_firstborn_scatter_nospiral.m
@@ -1,14 +1,14 @@
 function [output_sizes,zeroth_ranges] = prepare_firstborn_scatter_nospiral( ...
-    input_size,enabled_log2_resamplings,subscripts,nThetas,ranges)
+    data_size,enabled_log2_resamplings,subscripts,nThetas,ranges)
 %% Initialization of outputs
 nEnabled_gammas = length(enabled_log2_resamplings);
-is_deepest = ~iscell(input_size);
+is_deepest = ~iscell(data_size);
 is_oriented = nThetas>1;
 if is_deepest
     output_sizes = cell(nEnabled_gammas,1);
     zeroth_ranges = cell(nEnabled_gammas,1);
 else
-    cousin_size = drop_trailing(size(input_size,1));
+    cousin_size = drop_trailing(size(data_size,1));
     nCousins = prod(cousin_size);
     output_sizes = cell(nCousins,nEnabled_gammas);
     zeroth_ranges = cell(nCousins,nEnabled_gammas);
@@ -18,11 +18,11 @@ end
 if is_deepest && is_oriented
     % e.g. 2d scattering
     theta_range = [1;1;nThetas];
-    local_size = cat(2,input_size,nThetas);
+    local_size = cat(2,data_size,nThetas);
     local_range = cat(2,ranges{1+0},theta_range);
     for gamma_index = 1:nEnabled_gammas
         log2_resampling = enabled_log2_resamplings(gamma_index);
-        local_size(subscripts) = pow2(input_size(subscripts,log2_resampling));
+        local_size(subscripts) = pow2(data_size(subscripts,log2_resampling));
         output_sizes{gamma_index} = local_size;
         zeroth_ranges{gamma_index} = local_range;
         zeroth_ranges{gamma_index}(2,subscripts) = ...
@@ -46,7 +46,7 @@ elseif ~is_deepest && is_oriented
     theta_range = [1;1;nThetas];
     for cousin = 1:nCousins
         local_range = cat(2,ranges{1+0}{cousin},theta_range);
-        local_size = cat(2,input_size{cousin},nThetas);
+        local_size = cat(2,data_size{cousin},nThetas);
         for gamma_index = 1:nEnabled_gammas
             log2_resampling = enabled_log2_resamplings(gamma_index);
             output_sizes{cousin,gamma_index} = local_size;
