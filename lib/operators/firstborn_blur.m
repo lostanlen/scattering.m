fce419e3d418101da835134cd71e1312b9a74fc0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-13 13:07:24 +0200

    Update firstborn_blur with new spiraling

--- lib/operators/firstborn_blur.m
+++ lib/operators/firstborn_blur.m
@@ -1,10 +1,10 @@
 function [data,ranges] = firstborn_blur(data_ft,bank,ranges)
 %% Deep map across levels
 level_counter = length(ranges) - 1;
-input_sizes = drop_trailing(size(data_ft),1);
+input_size = drop_trailing(size(data_ft),1);
 if level_counter>0
     nNodes = numel(data_ft);
-    data = cell([input_sizes,1]);
+    data = cell([input_size,1]);
     for node = 1:nNodes
         % Recursive call
         ranges_node = get_ranges_node(ranges,node);
@@ -12,13 +12,15 @@ if level_counter>0
             firstborn_blur(data_ft{node},bank,ranges_node);
         ranges = set_ranges_node(ranges,ranges_node,node);
     end
-    return;
+    return
 end
 
 %% Selection of signal-adapted support for the filter bank
 bank_behavior = bank.behavior;
+colons = bank_behavior.colons;
 subscripts = bank_behavior.subscripts;
-support_index = 1 + log2(bank.spec.size/get_signal_support(data_ft,subscripts));
+signal_support = get_signal_support(data_ft,ranges,subscripts);
+support_index = 1 + log2(bank.spec.size/signal_support);
 phi = bank.phi{support_index};
 
 %% Definition of resampling factor
@@ -26,60 +28,64 @@ critical_log2_sampling = 1 - log2(bank.spec.T);
 log2_oversampling = bank_behavior.S.log2_oversampling;
 log2_resampling = critical_log2_sampling + log2_oversampling;
 
-%% Update of ranges at zeroth level (tensor level)
-ranges{1+0}(2,subscripts) = pow2(-log2_resampling);
-
-%% Initialization
-colons = bank_behavior.colons;
+%% Assignment preparation and update of ranges
 is_spiraled = isfield(bank_behavior,'spiral') && ...
     ~strcmp(get_suffix(bank_behavior.key),'j');
-
-%% Assignment preparation if spiraling is required
 if is_spiraled
     spiral = bank_behavior.spiral;
     nChromas = spiral.nChromas;
-    output_sizes = input_sizes;
-    resampled_sizes = pow2(output_sizes(subscripts),log2_resampling);
-    output_sizes(subscripts) = resampled_sizes;
-    resliced_sizes = output_sizes(spiral.subscript);
+    octave_padding_length = spiral.octave_padding_length;
+    spiral_subscript = spiral.subscript;
+    if length(subscripts)>1 || subscripts~=spiral_subscript
+        % there is no straightforward use case for this
+        error('spiraling only avaiable when subscripts==spiral_subscript');
+    end
+
+    % Definition of number of octaves
+    nSubscripts = length(input_size);
+    spiral_range = ranges{1+0}(:,spiral_subscript);
+    unpadded_nOctaves = ceil((spiral_range(3)-spiral_range(1)+1) / nChromas);
+    padded_nOctaves = pow2(nextpow2(unpadded_nOctaves + octave_padding_length));
+
+    % Definition of output_size
     resampled_nChromas = pow2(nChromas,log2_resampling);
-    nOctaves = pow2(nextpow2(spiral.octave_padding_length + ...
-        resliced_sizes/resampled_nChromas)-1);
-    nPadded_gammas = resampled_nChromas * nOctaves;
-    output_sizes(spiral.subscript) = nPadded_gammas;
-    spiraled_sizes = ...
-        [output_sizes(1:(spiral.subscript-1)), ...
-        resampled_nChromas,nOctaves, ...
-        output_sizes((spiral.subscript+1):end)];
-    subsasgn_structure.type = '()';
-    subsasgn_structure.subs = replicate_colon(1+length(input_sizes));
-    subsasgn_structure.subs(subscripts) = ...
-        arrayfun(@(x) 1:x,resampled_sizes,'UniformOutput',false);
-    zeroth_ranges = ranges{1+0};
-    first_father = zeroth_ranges(1,spiral.subscript);
-    zeroth_ranges(1,spiral.subscript) = 1 + mod(first_father-1,nChromas);
-    zeroth_ranges(3,spiral.subscript) = ...
-        zeroth_ranges(1,spiral.subscript) + nChromas - 1;
-    first_octave = 1 + floor(first_father/nChromas);
-    octave_range = [first_octave;1;first_octave+nOctaves-1];
-    zeroth_ranges = ...
-        [zeroth_ranges(:,1:(spiral.subscript)), ...
-        octave_range, ...
-        zeroth_ranges(:,(spiral.subscript+1:end))];
-    ranges(1+0) = {zeroth_ranges};
+    nPadded_gammas = resampled_nChromas * padded_nOctaves;
+    output_size = input_size;
+    output_size(subscripts) = nPadded_gammas;
+
+    % Definition of subsasgn_structure
+    resampled_length = pow2(input_size(subscripts),log2_resampling);
+    subsasgn_structure = substruct('()',replicate_colon(1+nSubscripts));
+    subsasgn_structure.subs{spiral_subscript} = 1:resampled_length;
+
+    % Definition of spiraled size
+    spiraled_size = ...
+        cat(2, output_size(1:(spiral_subscript-1)), ...
+        resampled_nChromas, padded_nOctaves, ...
+        output_size((spiral_subscript+1):end));
+
+    % Definition of range
+    first_father = ranges{1+0}(1,spiral_subscript);
+    first_chroma = 1 + mod(first_father,resampled_nChromas);
+    last_chroma = first_chroma + nChromas;
+    first_octave = 1 + floor(first_father/resampled_nChromas);
+    spiral_range = [first_chroma;pow2(-log2_resampling);last_chroma];
+    octave_range = [first_octave;1;first_octave+unpadded_nOctaves-1];
+    ranges{1+0} = cat(2, ranges{1+0}(:,1:(spiral_subscript-1)), ...
+        spiral_range, octave_range, ...
+        ranges{1+0}(:,(spiral_subscript+1):end));
+else
+    ranges{1+0}(2,subscripts) = pow2(-log2_resampling);
 end
 
 %% Blurring implementations
+%% []. Normal
 if ~is_spiraled
     data = ifft_multiply(data_ft,phi,log2_resampling,colons,subscripts);
 else
-    data = zeros(output_sizes);
-    data = subsasgn(data,subsasgn_structure, ...
+    %% S. Spiraled
+    data = subsasgn(zeros(output_size),subsasgn_structure, ...
         ifft_multiply(data_ft,phi,log2_resampling,colons,subscripts));
-end
-
-%% Spiraling if required
-if is_spiraled
-    data = reshape(data,spiraled_sizes);
+    data = reshape(data,spiraled_size);
 end
 end
