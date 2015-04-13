8dd1fda1f1e90370ce512d264d77573f05559613
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-13 18:16:12 +0200

    Write function dual_blur_dY

---
+++ lib/duality/dual_blur_dY.m
@@ -0,0 +1,39 @@
+function previous_sub_dY = dual_blur_dY(sub_dY,bank)
+%% Cell-wise map
+if iscell(sub_dY)
+    blur_handle = @(x) blur_Y(x,bank);
+    previous_sub_dY = map_unary(blur_handle,sub_dY);
+    return
+end
+
+%% Variable loading
+keys = sub_dY.keys;
+variable_tree = sub_dY.variable_tree;
+try
+    sibling = get_relatives(bank.behavior.key,variable_tree);
+catch err
+    if strcmp(err.identifier,'MATLAB:nonExistentField') && ...
+            strcmp(get_suffix(bank.behavior.key),'j')
+        sub_dY = unroll_spiral(sub_dY,bank);
+        variable_tree = sub_dY.variable_tree;
+        sibling = get_relatives(bank.behavior.key,variable_tree);
+    else
+        rethrow(err);
+    end
+end
+variable = get_leaf(variable_tree,bank.behavior.key);
+bank.behavior.subscripts = variable.subscripts;
+bank.behavior.colons = substruct('()',replicate_colon(length(keys{1+0})));
+
+%% Blurring
+if isempty(sibling)
+    previous_sub_dY.data_ft = ...
+        dual_firstborn_blur(sub_dY.data,bank,sub_dY.ranges);
+elseif sibling.is_firstborn
+    previous_sub_dY.data_ft= ...
+        dual_secondborn_blur(sub_dY.data,bank,sub_dY.ranges,sibling);
+else
+    previous_sub_dY.data = ...
+        dual_sibling_blur(sub_dY.data,bank,sub_dY.ranges,sibling);
+end
+end
