9d35ad03fea870c9aaec74e632515c60587e590d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-14 17:55:37 +0200

    Make copy_metadata modify its second argument

--- lib/backward/copy_metadata.m
+++ lib/backward/copy_metadata.m
@@ -1,9 +1,10 @@
-function layer_out = copy_metadata(layer_in)
+function layer_out = copy_metadata(layer_in,layer_out)
 %% Deep map across cells
 if iscell(layer_in)
     layer_out = cell(size(layer_in));
     for cell_index = 1:numel(layer_in)
-        layer_out{cell_index} = copy_metadata(layer_in{cell_index});
+        layer_out{cell_index} = ...
+            copy_metadata(layer_in{cell_index},layer_out{cell_index});
     end
 else
     layer_out.keys = layer_in.keys;
