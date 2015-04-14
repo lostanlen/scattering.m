a245ec9c27239cc298c1d46d5eb166ee105c7641
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-14 13:19:14 +0200

    Comment substract_data + change prototype in recursive call

--- lib/backward/substract_data.m
+++ lib/backward/substract_data.m
@@ -5,17 +5,25 @@ difference_ranges = cell(size(minuend_ranges));
 [difference_ranges{end},minuend_substruct,subtrahend_substruct] = ...
     intersect_ranges(minuend_ranges{end},subtrahend_ranges{end});
 
-%%
+%% Recursive substraction across levels
 if length(minuend_ranges)>1
+    % Subscripted reference
     minuend_data = subsref(minuend_data,minuend_substruct);
     subtrahend_data = subsref(subtrahend_data,subtrahend_substruct);
+    
+    % Difference initialization
     difference_data = cell(size(minuend_data));
+    
+    % Loop over nodes
     for node = 1:numel(minuend_data)
+        % Get respective ranges corresponding to node in minuend and subtrahend
         minuend_ranges_node = get_ranges_node(minuend_ranges,node);
         subtrahend_ranges_node = get_ranges_node(subtrahend_ranges,node);
+        % Recursive call
         [difference_data{node},difference_ranges_node] = substract_data( ...
-            minuend_data{node},minuend_ranges_node, ...
-            subtrahend_data{node},subtrahend_ranges_node);
+            minuend_data{node},subtrahend_data{node}, ...
+            minuend_ranges_node,subtrahend_ranges_node);
+        % Set ranges corresponding to node in difference
         difference_ranges = ...
             set_ranges_node(difference_ranges,difference_ranges_node,node);
     end
