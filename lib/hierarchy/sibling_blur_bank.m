18ef6dc81376943cb8b506cfb4e52f7ea9d8d051
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-15 22:34:41 +0200

    Remove obsolete functions

--- lib/hierarchy/sibling_blur_bank.m
+++
@@ -1,8 +0,0 @@
-function bank = sibling_blur_bank(bank,sibling)
-sibling_log2_samplings = [sibling.metas.log2_sampling];
-log2_oversampling = bank.behavior.S.log2_oversampling;
-critical_log2_resamplings = 1 - bank.spec.J - sibling_log2_samplings;
-bank.log2_resamplings = log2_oversampling + critical_log2_resamplings;
-bank.phi = bank.phi{1};
-bank.sibling.subscript = sibling.subscripts;
-end
