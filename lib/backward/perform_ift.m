ddc57d3faf2f632b23c42eefa38843f15c6b2301
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-15 09:39:13 +0200

    Write function perform_ft

---
+++ lib/backward/perform_ift.m
@@ -0,0 +1,13 @@
+function sub_Y = perform_ift(sub_Y,subscripts)
+%% Cell-wise map
+if iscell(sub_Y)
+    perform_ft_handle = @(x) perform_ft(x,subscripts);
+    sub_Y = map_unary(perform_ft_handle,sub_Y);
+    return
+end
+
+%% Multidimensional IFFT
+% TODO: avoid resorting to an anonymous handle
+ifft_handle = @(x) multidimensional_ifft(x,subscripts);
+sub_Y.data = map_unary(ifft_handle,sub_Y.data_ft);
+end
