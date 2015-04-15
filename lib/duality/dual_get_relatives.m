7d01c499b3e5e5f27f0cdc38080be87bedff97c9
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-15 16:54:41 +0200

    Write function dual_get_relatives

---
+++ lib/duality/dual_get_relatives.m
@@ -0,0 +1,42 @@
+function [sibling,uncle] = dual_get_relatives(key,variable_tree)
+%%
+head_name_cell = fieldnames(key);
+head_name = head_name_cell{1};
+head_relative_depth = length(key.(head_name));
+tail = key.(head_name){head_relative_depth};
+branch = variable_tree.(head_name){head_relative_depth};
+if isempty(tail)
+    if isfield(branch,'gamma') && length(branch.gamma)>1
+        sibling = branch.gamma{end-1}.leaf;
+        sibling.nSiblings = length(branch.gamma) - 2;
+    else
+        sibling = [];
+    end
+    uncle = [];
+    return
+end
+tail_head_name_cell = fieldnames(tail);
+tail_head_name = tail_head_name_cell{1};
+tail_head_relative_depth = length(tail.(tail_head_name));
+tail_tail = tail.(tail_head_name){tail_head_relative_depth};
+branch_branch = branch.(tail_head_name){tail_head_relative_depth};
+if isempty(tail_tail)
+    if strcmp(tail_head_name,'gamma') || strcmp(tail_head_name,'j')
+        if length(branch.gamma)>head_relative_depth
+            uncle = branch.gamma{head_relative_depth+1}.leaf;
+        else
+            uncle = [];
+        end
+    else
+        uncle = [];
+    end
+    if isfield(branch_branch,'gamma') && length(branch.gamma)>1
+        sibling = branch_branch.gamma{tail_head_relative_depth-1}.leaf;
+        sibling.nSiblings = length(branch_branch.gamma) - 2;
+    else
+        sibling = [];
+    end
+    return
+end
+[sibling,uncle] = dual_get_relatives(key,variable_tree_tail);
+end
