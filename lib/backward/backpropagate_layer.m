1c31a01c90768117d2e342f1871147d2afed11cf
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-16 15:12:43 +0200

    Unite single-variable and multi-variable cases in backpropagate_layer

--- lib/backward/backpropagate_layer.m
+++ lib/backward/backpropagate_layer.m
@@ -11,24 +11,27 @@ layer_dY{end} = ...
     backpropagate_nonlinearity(nonlinearity,layer_dU,layer_Y{end},layer_U);
 
 %% Multi-variable case: iterated one-variable backpropagation
-for variable_index = nVariables_to_transform:-1:2
-    bank = banks{variable_index};
-    sub_dY = layer_dY{1+variable_index};
-    nCells = 3^(variable_index-1);
-    previous_sub_dY = cell(nCells,1);
-    for cell_index = 1:nCells
-        if isempty(sub_dY{cell_index})
-            continue
+if nVariables_to_transform>1
+    for variable_index = nVariables_to_transform:-1:2
+        bank = banks{variable_index};
+        sub_dY = layer_dY{1+variable_index};
+        nCells = 3^(variable_index-1);
+        previous_sub_dY = cell(nCells,1);
+        for cell_index = 1:nCells
+            if isempty(sub_dY{cell_index})
+                continue
+            end
+            cell_dY = dual_blur_dY(sub_dY{nCells+cell_index},bank);
+            cell_dY = ...
+                copy_metadata(layer_Y{variable_index}{cell_index},cell_dY);
+            cell_dY = dual_scatter_dY(sub_dY{cell_index},bank,cell_dY);
+            previous_sub_dY{cell_index} = cell_dY;
         end
-        cell_dY = dual_blur_dY(sub_dY{nCells+cell_index},bank);
-        cell_dY = copy_metadata(layer_Y{variable_index}{cell_index},cell_dY);
-        cell_dY = dual_scatter_dY(sub_dY{cell_index},bank,cell_dY);
-        previous_sub_dY{cell_index} = cell_dY;
+        routing_sizes = [repmat(3,1,variable_index-1),1];
+        layer_dY{variable_index} = reshape(previous_sub_dY,routing_sizes);
     end
-    routing_sizes = repmat(3,1,variable_index);
-    layer_dY{variable_index} = reshape(previous_sub_dY,routing_sizes);
-    subscripts = bank.behavior.subscripts;
-    layer_dY{variable_index} = perform_ift(layer_dY{variable_index},subscripts);
+    layer_dY{1+1} = layer_dY{1+1}{1};
+    layer_Y{1+0} = layer_Y{1+0}{1};
 end
 
 %% Backpropagation of blurring/pooling operator in dS
@@ -37,5 +40,4 @@ layer_dY{1+0} = copy_metadata(layer_Y{1+0},layer_dY{1+0});
 
 %% Backpropagation of first variable
 layer_dY{1+0} = dual_scatter_dY(layer_dY{1+1},banks{1},layer_dY{1+0});
-layer_dY{1+0} = perform_ift(layer_dY{1+0},banks{1}.behavior.subscripts);
 end
