203ec3737da7e1aa453535729adfe3ef10209ec0
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-16 15:13:01 +0200

    Fix notation in dual_firstborn_blur

--- lib/duality/dual_firstborn_blur.m
+++ lib/duality/dual_firstborn_blur.m
@@ -1,7 +1,7 @@
 function data_ft = dual_firstborn_blur(data,bank,ranges)
 %% Deep map across levels
 level_counter = length(ranges) - 1;
-input_sizes = drop_trailing(size(data),1);
+input_size = drop_trailing(size(data),1);
 if level_counter>0
     nNodes = numel(data);
     data_ft = cell(nNodes,1);
@@ -10,8 +10,8 @@ if level_counter>0
         ranges_node = get_ranges_node(ranges,node);
         data_ft{node} = dual_firstborn_blur(data{node},bank,ranges_node);
     end
-    if length(input_sizes)>1
-        data_ft = reshape(data_ft,input_sizes);
+    if length(input_size)>1
+        data_ft = reshape(data_ft,input_size);
     end
     return
 end
