8dc41eefd13ea01ec555ba9e30bb2169b43a87b2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-17 14:13:51 +0200

    Tidy up fill_reconstruction_opt

--- lib/reconstruction/fill_reconstruction_opt.m
+++ lib/reconstruction/fill_reconstruction_opt.m
@@ -2,6 +2,8 @@ function reconstruction_opt = fill_reconstruction_opt(reconstruction_opt)
 if nargin<1
     reconstruction_opt = struct();
 end
+
+%% Method
 reconstruction_opt.method_string = ...
     default(reconstruction_opt,'method_string','momentum');
 reconstruction_opt.learning_rate = ...
@@ -10,20 +12,23 @@ switch reconstruction_opt.method_string
     case 'plain'
         reconstruction_opt.method.is_plain = true;
         reconstruction_opt.method.is_momentum = false;
-        reconstruction_opt.method.is_BFGS = false;
     case 'momentum'
         reconstruction_opt.method.is_plain = false;
         reconstruction_opt.method.is_momentum = true;
-        reconstruction_opt.method.is_BFGS = false;
         reconstruction_opt.momentum = ...
             default(reconstruction_opt,'momentum',0.9);
-    % TODO: include BFGS + line search
 end
-if isfield(reconstruction_opt,'regularizer')
-    reconstruction_opt.is_regularized = true;
-else
-    reconstruction_opt.is_regularized = false;
+
+%% Regularization
+reconstruction_opt.is_regularized = ...
+    default(reconstruction_opt,'is_regularized', ...
+    isfield(reconstruction_opt,'regularizer'));
+if reconstruction_opt.is_regularized
+    reconstruction_opt.regularized = ...
+        default(reconstruction_opt,'regularizer',0.1);
 end
+
+%% Soft thresholding
 if isfield(reconstruction_opt,'target_min') && ...
         isfield(reconstruction_opt,'target_max')
     reconstruction_opt.is_thresholded = true;
@@ -32,23 +37,21 @@ if isfield(reconstruction_opt,'target_min') && ...
 else
     reconstruction_opt.is_thresholded = false;
 end
-if isfield(reconstruction_opt,'verbosity_period')
-    reconstruction_opt.is_verbose = true;
-else
-    reconstruction_opt.is_verbose = false;
-end
-if isfield(reconstruction_opt,'spectrum_display_period')
-    reconstruction_opt.is_spectrum_displayed = true;
-else
-    reconstruction_opt.is_spectrum_displayed = false;
-end
-if reconstruction_opt.is_spectrum_displayed
-    reconstruction_opt.spectrum_display_period = ...
-        default(reconstruction_opt,'spectrum_display_period',10);
-end
+
+%% Verbosity
+reconstruction_opt.is_verbose = default(reconstruction_opt,'is_verbose',true);
 if reconstruction_opt.is_verbose && ...
         ~isfield(reconstruction_opt,'verbosity_period')
     reconstruction_opt.verbosity_period = 10;
+end
+
+%% Spectrum display
+reconstruction_opt.is_spectrum_displayed = ...
+    default(reconstruction_opt,'is_spectrum_displayed', ...
+    isfield(reconstruction_opt,'spectrum_display_period'));
+if reconstruction_opt.is_spectrum_displayed
+    reconstruction_opt.spectrum_display_period = ...
+        default(reconstruction_opt,'spectrum_display_period',10);
 end
 
 %% Alphanumeric ordering of field names
