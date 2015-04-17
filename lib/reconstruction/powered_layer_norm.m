f3dd1b7c08bcb2858e556d4714cfdc491590e960
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-17 14:12:06 +0200

    Write function powered_layer_norm

---
+++ lib/reconstruction/powered_layer_norm.m
@@ -0,0 +1,15 @@
+function powered_ellp_norm = powered_layer_norm(layer_S,norm_handle)
+%% Deep map across cells
+if iscell(layer_S)
+    for cell_index = 1:numel(layer_S)
+        if ~isempty(layer_S{cell_index})
+            powered_ellp_norm = powered_ellp_norm + ...
+                powered_layer_norm(layer_S{cell_index},norm_handle);
+        end
+    end
+    return
+end
+
+%% Call map reduce to sum powered norms of all paths
+powered_ellp_norm = mapreduce_unary(norm_handle,layer_S.data);
+end
