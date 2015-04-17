f9c89abdea4b8a4c60e4af36973dc28bd13874b3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-04-17 20:23:33 +0200

    Bug fix initialize powered_ellp_norm = 0

--- lib/reconstruction/powered_layer_norm.m
+++ lib/reconstruction/powered_layer_norm.m
@@ -1,6 +1,7 @@
 function powered_ellp_norm = powered_layer_norm(layer_S,norm_handle)
 %% Deep map across cells
 if iscell(layer_S)
+    powered_ellp_norm = 0;
     for cell_index = 1:numel(layer_S)
         if ~isempty(layer_S{cell_index})
             powered_ellp_norm = powered_ellp_norm + ...
