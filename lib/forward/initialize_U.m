dd07036f5d6de1ef2f7586794bc4a6bfc7fd4090
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-06-28 14:34:32 +0200

    write zero-padded chunking in initialize_U

--- lib/forward/initialize_U.m
+++ lib/forward/initialize_U.m
@@ -5,14 +5,10 @@ chunk_signal_size = bank_spec.size;
 tensor_size = size(tensor);
 subscripts = bank_behavior.subscripts;
 unpadded_signal_size = tensor_size(subscripts);
-if isinf(bank_spec.max_scale)
-    hop_signal_size = 2 * bank_spec.T;
-else
-    hop_signal_size = max_scale;
-end
+hop_signal_size = bank_spec.size / 2;
 nChunks = ceil(unpadded_signal_size./hop_signal_size);
-if nChunks>1 && isequal(subscripts,[1])
-    padded_signal_size = nChunks * bank_spec.size;
+if nChunks>2 && isequal(subscripts,1)
+    padded_signal_size = nChunks * hop_signal_size;
     padding_signal_size = padded_signal_size - unpadded_signal_size;
     if any(padding_signal_size>0)
         padding_size = tensor_size;
@@ -20,18 +16,27 @@ if nChunks>1 && isequal(subscripts,[1])
         padding_zeros = zeros(padding_size);
         tensor = cat(1,tensor,padding_zeros);
     end
-    if any(tensor_size(2:end) ~= 1)
-        chunked_tensor_size = ...
-            [chunk_signal_size,nChunks,tensor_subscripts(2:end)];
-    else
-        chunked_tensor_size = [chunk_signal_size,nChunks];
-    end
+    chunked_tensor_size = ...
+        drop_trailing([chunk_signal_size,nChunks,tensor_size(2:end)]);
     chunked_tensor = zeros(chunked_tensor_size);
-    for chunk_index = 1:nChunks
+    
+    % Special case: first chunk
+    lhs_indices = (chunk_signal_size/2+1):chunk_signal_size;
+    rhs_indices = 1:(chunk_signal_size/2);
+    chunked_tensor(lhs_indices,1,:) = tensor(rhs_indices,:);
+    
+    % General case
+    for chunk_index = 2:(nChunks-1)
         chunk_start = (chunk_index-1) * hop_signal_size + 1;
         chunk_end = chunk_start + chunk_signal_size - 1;
-        chunked_tensor(chunk_index,:,:) = tensor(chunk_start:chunk_end,:);
+        chunked_tensor(:,chunk_index,:) = tensor(chunk_start:chunk_end,:);
     end
+    
+    % Special case: last chunk
+    lhs_indices = 1:(chunk_signal_size/2);
+    rhs_indices = (chunk_signal_size/2+1):chunk_signal_size;
+    chunked_tensor(lhs_indices,nChunks,:) = tensor(rhs_indices,:);
+    
     tensor = chunked_tensor;
     variable_names = {'time','chunk'};
     U0 = initialize_variables_custom(chunked_tensor_size,variable_names);
