9cc19c7243b14c5ad5dcb7adad71d513de848515
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-06-30 04:42:35 +0200

    Bugfix drop_trailing

--- lib/utilities/drop_trailing.m
+++ lib/utilities/drop_trailing.m
@@ -2,6 +2,6 @@ function dropped_sizes = drop_trailing(sizes,min_dimension)
 if nargin<2
     dropped_sizes = sizes(1:find(sizes~=1,1,'last'));
 else
-    dropped_sizes = sizes(1:min(find(sizes~=1,1,'last'),min_dimension));
+    dropped_sizes = sizes(1:max(find(sizes~=1,1,'last'),min_dimension));
 end
 end
