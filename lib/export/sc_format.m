f1cd65493cb91fefbb4e3472942c23fed98f3944
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-07-07 12:14:34 +0200

    Unpad frequencies after time-freq scattering

--- lib/export/sc_format.m
+++ lib/export/sc_format.m
@@ -28,31 +28,42 @@ if iscell(layer_S)
     return
 end
 
-formatted_layer = format_data(layer_S.data, spatial_subscripts);
+formatted_layer = format_data(layer_S.data, spatial_subscripts, layer_S.ranges{1+0});
 end
 
-function formatted_data = format_data(data, spatial_subscripts)
+function formatted_data = format_data(data, spatial_subscripts, zeroth_ranges)
+%% Recursive call
 if iscell(data)
     nCells = numel(data);
     formatted_cells = cell(nCells,1);
     for cell_index = 1:nCells
         data_cell = data{cell_index};
-        formatted_cells{cell_index} = format_data(data_cell,spatial_subscripts);
+        ranges_cell = zeroth_ranges{cell_index};
+        formatted_cells{cell_index} = ...
+            format_data(data_cell,spatial_subscripts,ranges_cell);
     end
     formatted_data = [formatted_cells{:}];
     return
 end
 
+%% Unpadding
 input_sizes = size(data);
 nSubscripts = length(input_sizes);
 spatial_subscript_bools = true(nSubscripts,1);
+unpadded_sizes = zeros(nSubscripts,1);
+subsref_structure = substruct('()',cell(nSubscripts,1));
 for subscript_index = 1:nSubscripts
     spatial_subscript_bools(subscript_index) = ...
         any(spatial_subscripts == subscript_index);
+    range = zeroth_ranges(:,subscript_index);
+    unpadded_sizes(subscript_index) = ceil((range(3)-range(1)+1) / range(2));
+    subsref_structure.subs{subscript_index} = 1:unpadded_sizes(subscript_index);
 end
-spatial_sizes = input_sizes(spatial_subscripts);
-nonspatial_sizes = input_sizes(~spatial_subscript_bools);
-output_sizes = [prod(spatial_sizes), prod(nonspatial_sizes)];
+unpadded_data = subsref(data,subsref_structure);
 
-formatted_data = reshape(data, output_sizes);
+%% Reshaping to time-feature matrix
+spatial_sizes = unpadded_sizes(spatial_subscripts);
+nonspatial_sizes = unpadded_sizes(~spatial_subscript_bools);
+output_sizes = [prod(spatial_sizes), prod(nonspatial_sizes)];
+formatted_data = reshape(unpadded_data, output_sizes);
 end
