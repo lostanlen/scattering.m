41694b6241b1dd56d99b0471c45c5d312a8be895
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-25 12:22:35 +0200

    write bank_spec.is_phi_gaussian case

--- lib/filterbanks/generate_phi_ft.m
+++ lib/filterbanks/generate_phi_ft.m
@@ -9,11 +9,23 @@ switch signal_dimension
     case 1
         half_support_length = ...
             bank_spec.phi_bw_multiplier/2 * original_sizes/bank_spec.T;
-        half_support = 2:half_support_length;
-        symmetric_support = original_sizes + 1 - half_support + 1;
-        sqrt_truncated_remainder = sqrt(remainder(half_support));
-        phi_ft(half_support) = sqrt_truncated_remainder;
-        phi_ft(symmetric_support) = sqrt_truncated_remainder;
+        if bank_spec.is_phi_gaussian
+            denominator = half_support_length*half_support_length / log(2);
+            half_size = original_sizes / 2;
+            half_support = 2:half_size;
+            symmetric_support = original_sizes + 1 - half_support + 1;
+            omegas = half_support - 1;
+            half_gaussian = exp(- omegas .* omegas / denominator);
+            phi_ft(half_support) = half_gaussian;
+            phi_ft(symmetric_support) = half_gaussian;
+            phi_ft(half_size+1) = exp(- half_size*half_size / denominator);
+        else
+            half_support = 2:half_support_length;
+            symmetric_support = original_sizes + 1 - half_support + 1;
+            sqrt_truncated_remainder = sqrt(remainder(half_support));
+            phi_ft(half_support) = sqrt_truncated_remainder;
+            phi_ft(symmetric_support) = sqrt_truncated_remainder;
+        end
         % We enforce energy conservation for constant signals by setting
         % the Fourier transform to exactly 1 at frequency zero.
         phi_ft(1+0) = 1;
