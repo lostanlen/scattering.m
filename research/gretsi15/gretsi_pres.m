fa374a098f1b831a03c74b50d28204967f9e1481
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-07 16:24:19 +0200

    bug fix save

--- research/gretsi15/gretsi_pres.m
+++ research/gretsi15/gretsi_pres.m
@@ -184,7 +184,7 @@ while iteration < nIterations
             mod(iteration,reconstruction_opt.signal_display_period);
         if mod_iteration==0
             sc = display_scalogram(U{1+1});
-            save(['scalogram_it',int2str(iteration)], sc);
+            save(['scalogram_it',int2str(iteration)], 'sc');
             audiowrite(['cellospiral_it',int2str(iteration),'.wav',], signal, sample_rate);
         end
     end
