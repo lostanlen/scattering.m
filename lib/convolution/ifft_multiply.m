7988e6a2ad76e306c869b5e7d410e0a6a2e366aa
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-07 15:45:09 +0200

    rework ifft_multiply to fix mod_end<start case

--- lib/convolution/ifft_multiply.m
+++ lib/convolution/ifft_multiply.m
@@ -19,8 +19,12 @@ filter_start = filter_struct.ft_start;
 filter_end = filter_start + filter_sizes - 1;
 filter_mod_end = mod(filter_end + x_sizes/2 - 1, x_sizes) - x_sizes/2 + 1;
 
-pos_range_start = max(0,filter_start);
-if filter_mod_end>0
+if filter_mod_end<filter_start && filter_mod_end>0
+    pos_range_start = 0;
+else
+    pos_range_start = max(0, filter_start);
+end
+if filter_mod_end>0 && filter_mod_end>filter_start
     pos_range_end = min([x_end, filter_mod_end, y_end]);
 else
     pos_range_end = min([x_end, y_end]);
@@ -28,13 +32,19 @@ end
 pos_range = pos_range_start:pos_range_end;
 
 %% Definition of negative range
-neg_range_end = min(filter_mod_end, -1);
-if filter_start<0
+if filter_mod_end<filter_start && filter_mod_end>0
+    neg_range_end = -1;
+else
+    neg_range_end = min(filter_mod_end, -1);
+end
+if filter_mod_end<filter_start
+    neg_range_start = max([x_start, y_start]);
+elseif filter_start<0
     neg_range_start = max([x_start, filter_start, y_start]);
 elseif filter_mod_end<0
     neg_range_start = max([x_start, y_start]);
 else
-    neg_range_start = 1;
+    neg_range_start = 0;
 end
 neg_range = neg_range_start:neg_range_end;
 
@@ -46,7 +56,7 @@ colons.subs{subscripts} = x_range;
 trimmed_x_ft = subsref(x_ft,colons);
 
 %% Trimming of filter_ft if needed
-pos_filter_range = pos_range - filter_start + 1;
+pos_filter_range = mod(pos_range - filter_start + 1 - 1, x_sizes) + 1;
 neg_filter_range = mod(neg_range - filter_start + 1 - 1, x_sizes) + 1;
 filter_range = cat(2,neg_filter_range,pos_filter_range);
 colons.subs{subscripts} = filter_range;
