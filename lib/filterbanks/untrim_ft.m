5b7859bdec987cc60f71b1d918b65aeb53d7c7a5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-23 20:29:38 +0200

    fix indices in untrim_ft

--- lib/filterbanks/untrim_ft.m
+++ lib/filterbanks/untrim_ft.m
@@ -2,11 +2,11 @@ function coefficients = untrim_ft(filter,bank_spec)
 original_sizes = bank_spec.size;
 coefficients = zeros([original_sizes,1]);
 if ~isempty(filter.ft_pos)
-    coefficients(1 + (1:length(filter.ft_pos))) = ...
+    coefficients(1 + filter.ft_posfirst + (0:(length(filter.ft_pos)-1))) = ...
         filter.ft_pos;
 end
 if ~isempty(filter.ft_neg)
-    coefficients(end + filter.ft_neglast + ((-length(filter.ft_neg)+1):0)) = ...
+    coefficients(1 + end + filter.ft_neglast + ((-length(filter.ft_neg)+1):0)) = ...
         filter.ft_neg;
 end
 end
