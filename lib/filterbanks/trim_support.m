2bc10d0ef276d952df894cc93f03a1d0c87a7afd
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-23 12:47:45 +0200

    trim_support now splits Fourier domain into analytic and coanalytic parts

--- lib/filterbanks/trim_support.m
+++ lib/filterbanks/trim_support.m
@@ -1,4 +1,4 @@
-function [coefficients,start] = trim_support(coefficients,bank_spec)
+function [pos, posfirst, neg, neglast] = trim_support(coefficients,bank_spec)
 if sum(size(coefficients)~=1)>1
     error('support trimming not ready for multidimensional signals');
 end
@@ -6,8 +6,11 @@ end
 %% Does not trim if threshold is negative
 relative_threshold = bank_spec.trim_threshold;
 if relative_threshold<=0
-    start = 1;
-    return;
+    pos = coefficients((1+end/2):end);
+    posfirst = 1;
+    neg = coefficients(1:(end/2));
+    neglast = -1;
+    return
 end
 
 %% Finds maximum absolute value
@@ -17,24 +20,39 @@ abs2_values = coefficients .* conj(coefficients);
 max_indices = find(abs2_values==max_abs2);
 if length(max_indices)>1
     max_index = ...
-        floor(mod(sum(max_indices),original_length)/length(max_indices));
+        floor(mod(sum(max_indices), original_length)/length(max_indices));
 end
 absolute_threshold = max_abs2 * relative_threshold;
 
 %% Shifts circularly the absolute values
 shift_size = original_length/2 - max_index + 1;
-signal_colons = substruct('()',replicate_colon(length(bank_spec.size)));
+signal_colons = substruct('()', replicate_colon(length(bank_spec.size)));
 shifted_coefficients = ...
-    fast_circshift(coefficients,shift_size,signal_colons);
+    fast_circshift(coefficients, shift_size, signal_colons);
 shifted_abs2 = shifted_coefficients .* conj(shifted_coefficients);
 
 %% Detects first and last non-negligible coefficient in shifted domain
-first_detected_index = find(shifted_abs2>absolute_threshold,1);
-last_detected_index = find(shifted_abs2>absolute_threshold,1,'last');
+first_detected_index = find(shifted_abs2>absolute_threshold, 1);
+last_detected_index = find(shifted_abs2>absolute_threshold, 1, 'last');
 
-%% Trims coefficients to range
-coefficients = shifted_coefficients(first_detected_index:last_detected_index);
-start = first_detected_index + max_index - original_length/2 - 2;
+%% Gets starting point (between -N/2 and N/2-1)
+start = (first_detected_index-1) + (max_index-1) - original_length/2;
 start = ...
-    mod(start-1+original_length/2, original_length) - original_length/2 + 1;
+    mod(start+original_length/2, original_length) - original_length/2;
+
+%% Splits coefficients between analytic part (pos) and coanalytic part (neg)
+if start < 0
+    neg = shifted_coefficients(first_detected_index + (0:(1-start)));
+    neglast = -1;
+    pos = shifted_coefficients((1-start+1):last_detected_index);
+    if isempty(pos)
+        posfirst = [];
+    else
+        posfirst = 1;
+    end
+else
+    neg = [];
+    neglast = [];
+    pos = shifted_coefficients(first_detected_index:last_detected_index);
+    posfirst = start;
 end
