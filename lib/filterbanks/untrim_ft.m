ba4cb6ba6c8c114e1e2d96fbefe3c78527b34e84
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-23 18:53:32 +0200

    address isempty cases in untrim_ft

--- lib/filterbanks/untrim_ft.m
+++ lib/filterbanks/untrim_ft.m
@@ -1,6 +1,12 @@
 function coefficients = untrim_ft(filter,bank_spec)
 original_sizes = bank_spec.size;
 coefficients = zeros([original_sizes,1]);
-coefficients(1 + (1:length(filter.posfirst))) = filter.pos;
-coefficients(end + filter.neglast + ((-length(filter.neg)+1):0)) = filter.neg;
+if ~isempty(filter.ft_pos)
+    coefficients(1 + (1:length(filter.ft_pos))) = ...
+        filter.ft_pos;
+end
+if ~isempty(filter.ft_neg)
+    coefficients(end + filter.ft_neglast + ((-length(filter.ft_neg)+1):0)) = ...
+        filter.ft_neg;
+end
 end
