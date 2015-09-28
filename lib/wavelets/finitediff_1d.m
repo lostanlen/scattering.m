e2051cee90f8db12de1b282c9c5796050e792ef1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-09-28 17:51:45 +0200

    fix normalizers in finitediff_1d

--- lib/wavelets/finitediff_1d.m
+++ lib/wavelets/finitediff_1d.m
@@ -2,13 +2,13 @@ function finitediffs = finitediff_1d(~, bank_spec)
 finitediffs = zeros(bank_spec.size, 1);
 
 % First scale (first-order finite difference)
-finitediffs(end, 1) = -1 / sqrt(2);
+finitediffs(end, 1) = -1 / (sqrt(2) * sqrt(3));
 finitediffs(1, 1) = 0;
-finitediffs(2, 1) = 1 / sqrt(2);
+finitediffs(2, 1) = 1 / (sqrt(2) * sqrt(3));
 
 % Second scale (second-order finite difference)
-finitediffs(end, 2) = 1 / sqrt(6);
-finitediffs(1, 2) = -2 / sqrt(6);
-finitediffs(2, 2) = 1 / sqrt(6);
+finitediffs(end, 2) = 1 / (sqrt(6) * sqrt(3));
+finitediffs(1, 2) = -2 / (sqrt(6) * sqrt(3));
+finitediffs(2, 2) = 1 / (sqrt(6) * sqrt(3));
 end
 
