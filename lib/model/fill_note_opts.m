f5ca98974a86904eb29d5914f1005ba0511eb834
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-09 01:12:49 +0200

    default fundamental frequency is 110

--- lib/model/fill_note_opts.m
+++ lib/model/fill_note_opts.m
@@ -2,7 +2,7 @@ function note_opts = fill_note_opts(note_opts)
 note_opts.duration = ...
     default(note_opts, 'duration', 1.0); % in seconds
 note_opts.fundamental_frequency = ...
-    default(note_opts, 'fundamental_frequency', 440); % in Hz
+    default(note_opts, 'fundamental_frequency', 110); % in Hz
 note_opts.nPartials = default(note_opts, 'nPartials', 8);
 note_opts.sample_rate = default(note_opts, 'sample_rate', 22050);
 note_opts.spectral_exponent = default(note_opts, 'spectral_exponent', 0);
