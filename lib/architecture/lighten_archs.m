6a4c35417dd4992b9b62c6674e85d7b9621b0a7d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-14 15:23:40 +0200

    bugfix ligthen_archs bis

--- lib/architecture/lighten_archs.m
+++ lib/architecture/lighten_archs.m
@@ -5,11 +5,11 @@ for layer_index = 1:nLayers
     arch = archs{layer_index};
     nBanks = length(arch.banks);
     for bank_index = 1:nBanks
-        arch{bank_index} = rmfield(arch{bank_index}, 'psis');
-        arch{bank_index} = rmfield(arch{bank_index}, 'phi');
+        arch{bank_index} = rmfield(arch.banks{bank_index}, 'psis');
+        arch{bank_index} = rmfield(arch.banks{bank_index}, 'phi');
         if arch{bank_index}.spec.has_duals
-            arch{bank_index} = rmfield(arch{bank_index}, 'dual_psis');
-            arch{bank_index} = rmfield(arch{bank_index}, 'dual_phi');
+            arch{bank_index} = rmfield(arch.banks{bank_index}, 'dual_psis');
+            arch{bank_index} = rmfield(arch.banks{bank_index}, 'dual_phi');
         end
     end
     light_archs{layer_index} = arch;
