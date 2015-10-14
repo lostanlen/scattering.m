4be4fdec1e0029e0883a811fb6604dc1448f005a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-14 14:27:09 +0200

    write function lighten_archs

---
+++ lib/architecture/lighten_archs.m
@@ -0,0 +1,18 @@
+function light_archs = lighten_archs(archs)
+nLayers = length(archs);
+light_archs = archs;
+for layer_index = 1:nLayers
+    arch = archs{layer_index};
+    nBanks = length(banks);
+    for bank_index = 1:nBanks
+        arch{bank_index} = rmfield(arch{bank_index}, 'psis');
+        arch{bank_index} = rmfield(arch{bank_index}, 'phi');
+        if arch{bank_index}.spec.has_duals
+            arch{bank_index} = rmfield(arch{bank_index}, 'dual_psis');
+            arch{bank_index} = rmfield(arch{bank_index}, 'dual_phi');
+        end
+    end
+    light_archs{layer_index} = arch;
+end
+end
+
