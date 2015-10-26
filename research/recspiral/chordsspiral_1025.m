9d54807565f8da3b2aafb4e78cc0a00ebe5c6b1a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-26 11:24:47 +0100

    write some display in the chords spiral script

--- research/recspiral/chordsspiral_1025.m
+++ research/recspiral/chordsspiral_1025.m
@@ -35,12 +35,30 @@ end
 if strcmp(arch_type, 'spiral')
     opts{2}.j.invariance = 'bypassed';
     opts{2}.j.has_duals = true;
-    opts{2}.j.handle = @gammatone_1d;
+    opts{2}.j.handle = @finitediff_1d;
 end
 
 archs = sc_setup(opts);
 
-%% Options for the reconstruction$
+%% Display
+Y2 = display_secondorder_wavelets(archs, 0.5);
+%%
+gamma2 = 8;
+gamma_chroma = 8;
+gamma_octave = 2;
+theta_chroma = 2;
+theta_octave = 1;
+node = Y2{4}{1,1,1}.data{gamma2}{gamma_chroma, gamma_octave};
+tensor = node(:, :, :, theta_chroma, theta_octave);
+matrix = reshape(tensor, size(tensor, 1), size(tensor,2) * size(tensor, 3)).';
+imagesc(real(matrix));
+
+%%
+node = Y2{4}{1,1,2}.data{gamma2}{gamma_chroma};
+tensor = node(:, :, :, theta_chroma);
+matrix = reshape(tensor, size(tensor, 1), size(tensor, 2) * size(tensor, 3)).';
+imagesc(real(matrix));
+%% Options for the reconstruction
 reconstruction_opt = fill_reconstruction_opt(struct());
 
 %%
