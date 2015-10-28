f59489dd891d3db38758ab5057b36492c74f34f3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-28 14:18:16 +0100

    default invariance field in invariant_spec

--- lib/architecture/fill_invariant_spec.m
+++ lib/architecture/fill_invariant_spec.m
@@ -1,4 +1,13 @@
 function invariant_spec = fill_invariant_spec(opt)
 invariant_spec = struct();
+if isfield(opt, 'invariance')
+    invariant_spec = opt.invariance;
+end 
+if isfield(opt, 'T')
+    % TODO: integrate max pooling
+    enforce(invariant_spec, 'invariance', 'blurred');
+end
+if ~isfield(invariant_spec, 'invariance')
+    invariant_spec.invariance = 'none';
 end
 
