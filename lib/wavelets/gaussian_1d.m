07b027ffbeb10be6c212b7dc18921992521376aa
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-28 17:30:22 +0100

    gaussian_1d should output a column vector

--- lib/wavelets/gaussian_1d.m
+++ lib/wavelets/gaussian_1d.m
@@ -2,7 +2,7 @@ function gaussian = gaussian_1d(spec)
 %% Definition of half support length and haf size
 half_support_length = spec.T / 2;
 half_size = spec.size / 2;
-full_range = fftshift(-half_size:(half_size-1));
+full_range = fftshift(-half_size:(half_size-1)).';
 
 %% Definition of numerator and denominator in gaussian operand
 numerator = - full_range .* full_range;
