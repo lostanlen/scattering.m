15ef283b451e53a4b14b6350b0243dadd13f67d1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-09 15:52:37 +0100

    write blurred case in setup_invariant

--- lib/architecture/setup_invariant.m
+++ lib/architecture/setup_invariant.m
@@ -1,3 +1,8 @@
 function invariant = setup_invariant(invariant)
+if strcmp(invariant.spec, 'blurred')
+    phi_ift = invariant.spec.handle(invariant.spec);
+    phi_ft = multidimensional_fft(phi_ift,1:signal_dimension);
+    invariant.phi = optimize_bank(phi_ft,phi_ift,bank);
+end
 end
 
