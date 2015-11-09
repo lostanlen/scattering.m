4f04d091c4a56b53d46127474a9cb58f20ebe271
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-09 16:15:36 +0100

    update fill_bank_behavior, introduce is_phi_applied

--- lib/architecture/fill_bank_behavior.m
+++ lib/architecture/fill_bank_behavior.m
@@ -1,14 +1,11 @@
 function bank_behavior = fill_bank_behavior(opt)
 %%
-bank_behavior.U.is_blurred = default(opt,'is_U_blurred',true);
-bank_behavior.U.is_bypassed = default(opt,'is_U_bypassed',false);
-bank_behavior.U.is_scattered = default(opt,'is_U_scattered',true);
+bank_behavior.is_phi_applied = default(opt,'is_phi_applied',false);
 bank_behavior.U.log2_oversampling = default(opt,'U_log2_oversampling',1);
 signal_dimension = length(opt.size); % provided by user
 dimension = default(opt,'dimension',signal_dimension);
 bank_behavior.colons = substruct('()',replicate_colon(dimension));
 bank_behavior.dimension = dimension;
-bank_behavior.S.log2_oversampling = default(opt,'S_log2_oversampling',0);
 gamma_bounds = default(opt,'gamma_bounds',[1 Inf]);
 if length(gamma_bounds)==1
     gamma_bounds = [gamma_bounds gamma_bounds];
