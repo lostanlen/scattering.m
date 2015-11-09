b644a0887be3f8fecc009e43c48c551a1b6e7947
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-09 18:15:48 +0100

    call get_handle_dimension in setup_invariant

--- lib/architecture/setup_invariant.m
+++ lib/architecture/setup_invariant.m
@@ -1,7 +1,8 @@
 function invariant = setup_invariant(invariant)
-if strcmp(invariant.spec, 'blurred')
+if strcmp(invariant.spec.invariance, 'blurred')
     phi_ift = invariant.spec.handle(invariant.spec);
-    phi_ft = multidimensional_fft(phi_ift,1:signal_dimension);
+    dimension = get_handle_dimension(invariant.spec.handle);
+    phi_ft = multidimensional_fft(phi_ift,1:dimension);
     invariant.phi = optimize_bank(phi_ft,phi_ift,bank);
 end
 end
