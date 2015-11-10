b76a22e0f27ea9b6d0746f360d01ed9d50d61487
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 22:52:10 +0100

    define is_invariant and is_bypassed fields in fill_invariant_behavior

--- lib/architecture/fill_invariant_behavior.m
+++ lib/architecture/fill_invariant_behavior.m
@@ -1,4 +1,5 @@
 function behavior = fill_invariant_behavior(opt)
-
+behavior.S.is_invariant = default(opt, 'is_S_invariant', true);
+behavior.S.is_bypassed = default(opt, 'is_S_bypassed', false);
 end
 
