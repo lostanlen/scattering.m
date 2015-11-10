c56019acd36383bb5639b3248b5994610d76606f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 22:54:01 +0100

    define subscripts field in fill_invariant_behavior, add second input arg

--- lib/architecture/fill_invariant_behavior.m
+++ lib/architecture/fill_invariant_behavior.m
@@ -1,5 +1,10 @@
-function behavior = fill_invariant_behavior(opt)
+function behavior = fill_invariant_behavior(opt, bank_behavior)
 behavior.S.is_invariant = default(opt, 'is_S_invariant', true);
 behavior.S.is_bypassed = default(opt, 'is_S_bypassed', false);
+if nargin<2
+    behavior.subscripts = default(opt, 'subscripts', bank_behavior);
+else
+    behavior.subscripts = opt.subscripts;
+end
 end
 
