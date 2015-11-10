22ef4f5752fb2e252676402156ab2783ce03138a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 23:53:27 +0100

    define default fields key and subscripts in fill_invariant_behavior

--- lib/architecture/fill_invariant_behavior.m
+++ lib/architecture/fill_invariant_behavior.m
@@ -2,8 +2,10 @@ function behavior = fill_invariant_behavior(opt, bank_behavior)
 behavior.S.is_invariant = default(opt, 'is_S_invariant', true);
 behavior.S.is_bypassed = default(opt, 'is_S_bypassed', false);
 if nargin<2
-    behavior.subscripts = default(opt, 'subscripts', bank_behavior);
+    behavior.key = default(opt, 'key', bank_behavior.key);
+    behavior.subscripts = default(opt, 'subscripts', bank_behavior.key);
 else
+    behavior.key = opt.key;
     behavior.subscripts = opt.subscripts;
 end
 end
