c50dc151e280d44d108cf0431fae38fc92033240
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 14:49:57 +0100

    update Y_to_S to disentangle banks from invariants

--- lib/forward/Y_to_S.m
+++ lib/forward/Y_to_S.m
@@ -1,8 +1,9 @@
 function layer_S = Y_to_S(layer_Y,arch)
 %% Initialization
-nVariables_to_transform = length(arch.banks);
 banks = arch.banks;
+nBanks = length(banks);
 invariants = arch.invariants;
+nInvariants = length(invariants);
 % This boolean is true at the last layer
 is_U_bypassed = (length(layer_Y)==1);
 % This boolean is true at the first layer (except for videos)
@@ -11,11 +12,15 @@ is_U_single_scattered = nVariables_to_transform==1 && ...
     ~banks{1}.behavior.U.is_blurred && ...
     ~banks{1}.behavior.U.is_bypassed;
 
+invariant_booleans = cellfun(@(x) x.behavior.S.is_invariant, invariants);
+bypassed_booleans = cellfun(@(x) x.behavior.S.is_bypassed, invariants);
+
 if is_U_bypassed || is_U_single_scattered
     layer_S = layer_Y{1};
     start_index = 1;
 else
-    blurring_booleans = cellfun(@(x) x.behavior.S.is_blurred,banks);
+    blurring_booleans = invariant_booleans & ...
+        cellfun(@(x) strcmp(x.spec.invariance, 'blurred'), invariants);
     coordinates = 2 + ~blurring_booleans;
     start_index = 2;
     condition = true;
@@ -38,11 +43,16 @@ else
     end
 end
 
+%% Check that S defines a single invariant
+has_multiple_invariants = any(invariant_booleans & bypassed_booleans);
+if has_multiple_invariants
+    error('Multiple invariants in Y_to_S not ready yet');
+end
+
 %% Iterated one-variable blurring or pooling
 for variable_index = start_index:nVariables_to_transform
-    bank = banks{variable_index};
     invariant = invariants{variable_index};
-    if strcmp(invariant.spec.invariant, 'blurred')
+    if strcmp(invariant.spec.invariance, 'blurred')
         if iscell(layer_S)
             subsref_structure.type = '()';
             subsref_structure.subs = cat(2, ...
@@ -56,10 +66,8 @@ for variable_index = start_index:nVariables_to_transform
             layer_S = perform_ft(layer_S,bank.behavior.key);
             layer_S = blur_Y(layer_S,bank);
         end
-    elseif bank.behavior.S.is_pooled
+    elseif strcmp(invariant.spec.invariance, 'pooled');
         error('Nonlinear pooling not ready yet');
-        %pooling = arch.poolings{variable_index};
-        %layer_S = pool_Y(layer_S,pooling);
     end
 end
 end
