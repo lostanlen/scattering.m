47c0e1bee618094151320afdebfc158a39d75c9b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 22:51:48 +0100

    rename bank_behavior -> behavior in fill_bank_behavior

--- lib/architecture/fill_bank_behavior.m
+++ lib/architecture/fill_bank_behavior.m
@@ -1,27 +1,27 @@
 function bank_behavior = fill_bank_behavior(opt)
 %%
-bank_behavior.U.is_blurred = default(opt,'is_U_blurred',true);
-bank_behavior.U.is_bypassed = default(opt,'is_U_bypassed',false);
-bank_behavior.U.is_scattered = default(opt,'is_U_scattered',true);
-bank_behavior.U.log2_oversampling = default(opt,'U_log2_oversampling',1);
+behavior.U.is_blurred = default(opt,'is_U_blurred',true);
+behavior.U.is_bypassed = default(opt,'is_U_bypassed',false);
+behavior.U.is_scattered = default(opt,'is_U_scattered',true);
+behavior.U.log2_oversampling = default(opt,'U_log2_oversampling',1);
 signal_dimension = length(opt.size); % provided by user
 dimension = default(opt,'dimension',signal_dimension);
-bank_behavior.colons = substruct('()',replicate_colon(dimension));
-bank_behavior.dimension = dimension;
-bank_behavior.S.log2_oversampling = default(opt,'S_log2_oversampling',0);
+behavior.colons = substruct('()',replicate_colon(dimension));
+behavior.dimension = dimension;
+behavior.S.log2_oversampling = default(opt,'S_log2_oversampling',0);
 gamma_bounds = default(opt,'gamma_bounds',[1 Inf]);
 if length(gamma_bounds)==1
     gamma_bounds = [gamma_bounds gamma_bounds];
 end
-bank_behavior.gamma_bounds = gamma_bounds;
-bank_behavior.is_sibling_padded = default(opt,'is_sibling_padded',false);
-bank_behavior.is_parallel = default(opt,'is_parallel',false);
-bank_behavior.key = opt.key; % provided in caller parse_plans
-bank_behavior.output_dimension = opt.output_dimension; % provided in caller
-bank_behavior.padding = parse_padding(default(opt,'padding','periodic'));
-bank_behavior.sibling_mask_factor = default(opt,'sibling_mask_factor',1);
+behavior.gamma_bounds = gamma_bounds;
+behavior.is_sibling_padded = default(opt,'is_sibling_padded',false);
+behavior.is_parallel = default(opt,'is_parallel',false);
+behavior.key = opt.key; % provided in caller parse_plans
+behavior.output_dimension = opt.output_dimension; % provided in caller
+behavior.padding = parse_padding(default(opt,'padding','periodic'));
+behavior.sibling_mask_factor = default(opt,'sibling_mask_factor',1);
 if isfield(opt,'spiral')
-    bank_behavior.spiral = opt.spiral;
+    behavior.spiral = opt.spiral;
 end
-bank_behavior.subscripts = opt.subscripts; % provided in caller setup_plans
+behavior.subscripts = opt.subscripts; % provided in caller setup_plans
 end
