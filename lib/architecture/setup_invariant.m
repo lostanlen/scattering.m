7d07f56957a151f4a432bb20524a546bccb6556e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 23:17:55 +0100

    take real part of phi_ft if invariant.spec.has_real_ft

--- lib/architecture/setup_invariant.m
+++ lib/architecture/setup_invariant.m
@@ -3,6 +3,9 @@ if strcmp(invariant.spec.invariance, 'blurred')
     phi_ift = invariant.spec.handle(invariant.spec);
     dimension = get_handle_dimension(invariant.spec.handle);
     phi_ft = multidimensional_fft(phi_ift,1:dimension);
+    if invariant.spec.has_real_ft
+        phi_ft = real(phi_ft);
+    end
     invariant.phi = optimize_bank(phi_ft,phi_ift,invariant);
 end
 end
