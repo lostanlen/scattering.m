c4ddc1bcc7019d617ea888df7f151c7c410e9e43
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 23:01:30 +0100

    rename bank_spec to spec in trim_ft

--- lib/filterbanks/trim_ft.m
+++ lib/filterbanks/trim_ft.m
@@ -1,10 +1,10 @@
-function filter_struct = trim_ft(coefficients,bank_spec)
+function filter_struct = trim_ft(coefficients,spec)
 if sum(size(coefficients)~=1)>1
     error('support trimming not ready for multidimensional signals');
 end
 
 %% Does not trim if threshold is negative
-relative_threshold = bank_spec.trim_threshold;
+relative_threshold = spec.trim_threshold;
 if relative_threshold<=0
     filter_struct.ft_pos = coefficients(1:(end/2));
     filter_struct.ft_posfirst = 0;
@@ -18,9 +18,9 @@ abs2_coefficients = coefficients .* conj(coefficients);
 
 %% Detects first and last non-negligible coefficient in shifted domain
 first_detected_index = ...
-    find(abs2_coefficients>bank_spec.trim_threshold, 1);
+    find(abs2_coefficients>spec.trim_threshold, 1);
 last_detected_index = ...
-    find(abs2_coefficients>bank_spec.trim_threshold, 1, 'last');
+    find(abs2_coefficients>spec.trim_threshold, 1, 'last');
 
 %% Get coanalytic part (negative frequencies)
 full_length = length(coefficients);
