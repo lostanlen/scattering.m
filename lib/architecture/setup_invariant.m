d1f9936d4972e15911d1dcd8aee91c14dcfa0ebe
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 14:50:18 +0100

    update setup_invariant

--- lib/architecture/setup_invariant.m
+++ lib/architecture/setup_invariant.m
@@ -1,9 +1,10 @@
-function invariant = setup_invariant(invariant)
+function invariant = setup_invariant(invariant,bank)
+invariant.behavior.subscripts = bank.subscripts;
 if strcmp(invariant.spec.invariance, 'blurred')
     phi_ift = invariant.spec.handle(invariant.spec);
     dimension = get_handle_dimension(invariant.spec.handle);
     phi_ft = multidimensional_fft(phi_ift,1:dimension);
-    invariant.phi = optimize_bank(phi_ft,phi_ift,bank);
+    invariant.phi = optimize_bank(phi_ft,phi_ift,invariant);
 end
 end
 
