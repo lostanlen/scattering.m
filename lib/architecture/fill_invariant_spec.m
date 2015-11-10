074a4a7652daa73c05664823ef5177bd17734aca
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-10 16:35:54 +0100

    update fill_invariant_spec

--- lib/architecture/fill_invariant_spec.m
+++ lib/architecture/fill_invariant_spec.m
@@ -1,26 +1,30 @@
 function invariant_spec = fill_invariant_spec(opt, bank_spec)
-invariant_spec = struct('size', bank_spec.size);
-if isfield(opt, 'invariance')
-    invariant_spec.invariance = opt.invariance;
-end 
-if isfield(opt, 'T')
-    % TODO: integrate max pooling
-    enforce(invariant_spec, 'invariance', 'blurred');
-    invariant_spec.T = opt.T;
-end
-if ~isfield(invariant_spec, 'invariance')
-    invariant_spec.invariance = 'none';
-end
-if strcmp(invariant_spec.invariance, 'blurred')
-    switch func2str(bank_spec.wavelet_handle)
-        case 'morlet_1d'
-            invariant_handle = @gaussian_1d;
-        case 'gammatone_1d'
-            invariant_handle = @gamma_1d;
-        case 'finitediff_1d'
-            invariant_handle = @rectangular_1d;
+%% Specify default invariant
+invariant_spec.invariance = default(opt, 'invariance', 'blurred');
+
+%% If a bank specification is available, import size and default T
+% The default invariant blurring handle is also specified
+if nargin<2
+    invariant_spec = opt;
+    if strcmp(invariant_spec.invariance, 'blurred')
+        invariant_spec.handle = ...
+            default(opt, 'invariant_handle', @gaussian_1d);
     end
-    invariant_spec.handle = default(opt, 'invariant_handle', invariant_handle);
+else
+    invariant_spec = struct('size', bank_spec.size);
+    invariant_spec.T = default(opt, 'T', bank_spec.T);
+    if strcmp(invariant_spec.invariance, 'blurred')
+        switch func2str(bank_spec.wavelet_handle)
+            case 'morlet_1d'
+                invariant_handle = @gaussian_1d;
+            case 'gammatone_1d'
+                invariant_handle = @gamma_1d;
+            case 'finitediff_1d'
+                invariant_handle = @rectangular_1d;
+        end
+        invariant_spec.handle = ...
+            default(opt, 'invariant_handle', invariant_handle);
+    end
 end
 invariant_spec.phi_bw_multiplier = ...
     default(invariant_spec, 'phi_bw_multiplier', bank_spec.phi_bw_multiplier);
