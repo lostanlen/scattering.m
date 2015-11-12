50b18ebbffa59eaad6991371120fdd6c8071baf1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-12 16:06:39 +0100

    write function gamma_1d

---
+++ lib/wavelets/gamma_1d.m
@@ -0,0 +1,16 @@
+function phi_ift = gamma_1d(spec)
+gamma_order = 1.2;
+standard_deviation_multiplier = 0.5;
+standard_deviation = spec.T/2 * standard_deviation_multiplier;
+alpha = sqrt(gamma_order) / standard_deviation;
+full_range = (1:spec.size).';
+monomial = full_range.^(gamma_order - 1);
+exponential = exp(- alpha * full_range);
+phi_ift = monomial .* exponential;
+[~, maximum_index] = max(abs(phi_ift));
+time_shift = 1 - maximum_index;
+phi_ift = circshift(phi_ift, time_shift);
+phi_ift = phi_ift / norm(phi_ift);
+normalizer = max(abs(phi_ft));
+phi_ift = phi_ift / normalizer;
+end
