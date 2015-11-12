39fb4193dd73bdebd3c198370cc0e3da619460dc
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-12 15:14:17 +0100

    whitespace in fill_bank_behavior

--- lib/architecture/fill_bank_behavior.m
+++ lib/architecture/fill_bank_behavior.m
@@ -1,25 +1,25 @@
 function behavior = fill_bank_behavior(opt)
 %%
-behavior.U.is_blurred = default(opt,'is_U_blurred',true);
-behavior.U.is_bypassed = default(opt,'is_U_bypassed',false);
-behavior.U.is_scattered = default(opt,'is_U_scattered',true);
-behavior.U.log2_oversampling = default(opt,'U_log2_oversampling',1);
+behavior.U.is_blurred = default(opt, 'is_U_blurred', true);
+behavior.U.is_bypassed = default(opt, 'is_U_bypassed', false);
+behavior.U.is_scattered = default(opt, 'is_U_scattered', true);
+behavior.U.log2_oversampling = default(opt, 'U_log2_oversampling', 1);
 signal_dimension = length(opt.size); % provided by user
-dimension = default(opt,'dimension',signal_dimension);
-behavior.colons = substruct('()',replicate_colon(dimension));
+dimension = default(opt, 'dimension', signal_dimension);
+behavior.colons = substruct('()', replicate_colon(dimension));
 behavior.dimension = dimension;
-behavior.S.log2_oversampling = default(opt,'S_log2_oversampling',0);
-gamma_bounds = default(opt,'gamma_bounds',[1 Inf]);
+behavior.S.log2_oversampling = default(opt, 'S_log2_oversampling', 0);
+gamma_bounds = default(opt, 'gamma_bounds', [1 Inf]);
 if length(gamma_bounds)==1
     gamma_bounds = [gamma_bounds gamma_bounds];
 end
 behavior.gamma_bounds = gamma_bounds;
-behavior.is_sibling_padded = default(opt,'is_sibling_padded',false);
-behavior.is_parallel = default(opt,'is_parallel',false);
+behavior.is_sibling_padded = default(opt, 'is_sibling_padded', false);
+behavior.is_parallel = default(opt, 'is_parallel', false);
 behavior.key = opt.key; % provided in caller setup_plans
 behavior.output_dimension = opt.output_dimension; % provided in caller
-behavior.padding = parse_padding(default(opt,'padding','periodic'));
-behavior.sibling_mask_factor = default(opt,'sibling_mask_factor',1);
+behavior.padding = parse_padding(default(opt, 'padding', 'periodic'));
+behavior.sibling_mask_factor = default(opt, 'sibling_mask_factor', 1);
 if isfield(opt,'spiral')
     behavior.spiral = opt.spiral;
 end
