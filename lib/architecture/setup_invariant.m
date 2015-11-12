5414338e41e4665c3c4c827e4783fb7388d8cc20
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-12 17:25:57 +0100

    rename handle to invariant_handle

--- lib/architecture/setup_invariant.m
+++ lib/architecture/setup_invariant.m
@@ -1,7 +1,7 @@
 function invariant = setup_invariant(invariant)
 if strcmp(invariant.spec.invariance, 'blurred')
-    phi_ift = invariant.spec.handle(invariant.spec);
-    dimension = get_handle_dimension(invariant.spec.handle);
+    phi_ift = invariant.spec.invariant_handle(invariant.spec);
+    dimension = get_handle_dimension(invariant.spec.invariant_handle);
     phi_ft = multidimensional_fft(phi_ift,1:dimension);
     if invariant.spec.has_real_ft
         phi_ft = real(phi_ft);
