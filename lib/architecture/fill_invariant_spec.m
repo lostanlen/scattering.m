56a372f74d778f1372aeeaa82e783b723375883a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-12 17:38:03 +0100

    fill_invariant_spec now calls default_invariant_handle

--- lib/architecture/fill_invariant_spec.m
+++ lib/architecture/fill_invariant_spec.m
@@ -18,17 +18,8 @@ else
     spec.size = bank_spec.size;
     spec.T = default(opt, 'T', bank_spec.T);
     if strcmp(spec.invariance, 'blurred')
-        switch func2str(bank_spec.wavelet_handle)
-            case 'morlet_1d'
-                invariant_handle = @gaussian_1d;
-            case 'gammatone_1d'
-                invariant_handle = @gamma_1d;
-            case 'finitediff_1d'
-                invariant_handle = @rectangular_1d;
-                spec.J = bank_spec.J;
-        end
-        spec.invariant_handle = ...
-            default(opt, 'invariant_handle', invariant_handle);
+        spec.invariant_handle = default(opt, 'invariant_handle', ...
+            default_invariant_handle(bank_spec.wavelet_handle));
         spec.phi_bw_multiplier = ...
             default(spec, 'phi_bw_multiplier', ...
             bank_spec.phi_bw_multiplier);
