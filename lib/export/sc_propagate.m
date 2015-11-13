77388c7abbdcb3ae647067225f64e3434fcdadfc
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 11:17:22 +0100

    update sc_propagate

--- lib/export/sc_propagate.m
+++ lib/export/sc_propagate.m
@@ -2,9 +2,9 @@ function [S, U, Y] = sc_propagate(signal, archs)
 %% Initialization of networks S, U, and Y.
 % S and U are zero-based ; Y is one-based.
 nLayers = length(archs);
-S = cell(1, 1 + nLayers);
-U = cell(1, 1 + nLayers);
-Y = cell(1, 1 + nLayers);
+S = cell(1, nLayers);
+U = cell(1, nLayers);
+Y = cell(1, nLayers);
 
 U{1+0} = initialize_U(signal, archs{1}.banks{1});
 
@@ -13,11 +13,17 @@ for layer = 1:nLayers
     arch = archs{layer};
     previous_layer = layer - 1;
     % Scatter iteratively layer U to get sub-layers Y
-    Y{layer} = U_to_Y(U{1+previous_layer}, arch);
+    if isfield(arch, 'banks')
+        Y{layer} = U_to_Y(U{1 + previous_layer}, arch.banks);
+    else
+        Y{layer} = U{1 + previous_layer};
+    end
     % Apply non-linearity to last sub-layer Y to get layer U
-    U{1+layer} = Y_to_U(Y{layer}{end}, arch);
+    U{1+layer} = Y_to_U(Y{layer}{end}, arch.nonlinearity);
     % Blur/pool first sub-layer Y to get layer S
-    S{1+previous_layer} = Y_to_S(Y{layer}, arch);
+    if isfield(arch, 'invariants')
+        S{1+previous_layer} = Y_to_S(Y{layer}, arch);
+    end
 end
 
 %% Unchunk if necessary
