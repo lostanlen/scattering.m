3ce5c292fae69279f616918a5b893606d82370c7
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 14:08:57 +0100

    cell-wise map in map_collapse_ranges

--- lib/map/map_collapse_ranges.m
+++ lib/map/map_collapse_ranges.m
@@ -1,2 +1,13 @@
 function zeroth_ranges_out = map_collapse_ranges(zeroth_ranges_in)
+%% Cell-wise map
+if iscell(zeroth_range_in)
+    cell_sizes = size(zeroth_ranges_in);
+    zeroth_ranges_out = cell(cell_sizes);
+    nCells = prod(cell_sizes);
+    for cell_index = 1:nCells
+        zeroth_ranges_out{cell_index} = ...
+            map_collapse_ranges(zeroth_ranges_in{cell_index});
+    end
+end
+
 end
