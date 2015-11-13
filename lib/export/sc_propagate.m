d751b389a5ccb6c5d54def88bb0a9e8ee9fac569
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 10:46:49 +0100

    sc_propagate now complies with custom invariants (backwards-compatible)

--- lib/export/sc_propagate.m
+++ lib/export/sc_propagate.m
@@ -1,30 +1,24 @@
-function [S,U,Y] = sc_propagate(signal,archs)
-%% Initialization of networks S, U and Y.
+function [S, U, Y] = sc_propagate(signal, archs)
+%% Initialization of networks S, U, and Y.
 % S and U are zero-based ; Y is one-based.
 nLayers = length(archs);
-S = cell(1,1+nLayers);
-U = cell(1,1+nLayers);
-Y = cell(1,1+nLayers);
+S = cell(1, 1 + nLayers);
+U = cell(1, 1 + nLayers);
+Y = cell(1, 1 + nLayers);
 
-U{1+0} = initialize_U(signal,archs{1}.banks{1});
+U{1+0} = initialize_U(signal, archs{1}.banks{1});
 
 %% Propagation cascade
 for layer = 1:nLayers
     arch = archs{layer};
     previous_layer = layer - 1;
     % Scatter iteratively layer U to get sub-layers Y
-    Y{layer} = U_to_Y(U{1+previous_layer},arch);
+    Y{layer} = U_to_Y(U{1+previous_layer}, arch);
     % Apply non-linearity to last sub-layer Y to get layer U
-    U{1+layer} = Y_to_U(Y{layer}{end},arch);
+    U{1+layer} = Y_to_U(Y{layer}{end}, arch);
     % Blur/pool first sub-layer Y to get layer S
-    S{1+previous_layer} = Y_to_S(Y{layer},arch);
+    S{1+previous_layer} = Y_to_S(Y{layer}, arch);
 end
-
-%% Use last bank to compute last layer S
-% Note that the last banks are used to compute penultimate layer S
-% (see loop above) as well as last layer S.
-Y{1+nLayers}{1+0} = initialize_Y(U{1+nLayers},arch.banks);
-S{1+nLayers} = Y_to_S(Y{1+nLayers},arch);
 
 %% Unchunk if necessary
 S = sc_unchunk(S);
