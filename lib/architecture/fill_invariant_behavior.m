68a54e98650eb8a698303779d569c16fb425f921
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 10:24:24 +0100

    bugfix fill_invariant_behavior

--- lib/architecture/fill_invariant_behavior.m
+++ lib/architecture/fill_invariant_behavior.m
@@ -2,16 +2,16 @@ function behavior = fill_invariant_behavior(opt, bank_behavior)
 behavior.S.is_invariant = default(opt, 'is_S_invariant', true);
 behavior.S.is_bypassed = default(opt, 'is_S_bypassed', false);
 if nargin<2
+    behavior.dimension = opt.dimension;
+    behavior.key = opt.key;
+    behavior.output_dimension = opt.output_dimension;
+    behavior.subscripts = opt.subscripts;
+else
     behavior.dimension = default(opt, 'dimension', bank_behavior.dimension);
     behavior.key = default(opt, 'key', bank_behavior.key);
     behavior.subscripts = default(opt, 'subscripts', bank_behavior.subscripts);
     behavior.output_dimension = default(opt, 'output_dimension', ...
         bank_behavior.output_dimension);  
-else
-    behavior.dimension = opt.dimension;
-    behavior.key = opt.key;
-    behavior.output_dimension = opt.output_dimension;
-    behavior.subscripts = opt.subscripts;
 end
 behavior.S.log2_oversampling = default(opt, 'S_log2_oversampling', 0);
 
