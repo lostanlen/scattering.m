5adb567e81fc55865943ace40919bb6173a2180d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 13:44:45 +0100

    write recursion in map_sum

--- lib/map/map_sum.m
+++ lib/map/map_sum.m
@@ -1,2 +1,11 @@
 function data_out = map_sum(data_in, subscripts)
+%% Cell-wise map
+if iscell(data_in)
+    sizes = size(data_in);
+    data_out = cell(sizes);
+    nCells = prod(sizes);
+    for cell_index = 1:nCells
+        data_out{cell_index} = map_sum(data_in, subscripts);
+    end
+    return
 end
