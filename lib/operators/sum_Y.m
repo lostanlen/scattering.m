ec6f7cb91ecd812da8154d135f2fee43369b71f4
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 13:36:25 +0100

    cell-wise map in sum_Y function

--- lib/operators/sum_Y.m
+++ lib/operators/sum_Y.m
@@ -1,4 +1,11 @@
 function next_sub_Y = sum_Y(sub_Y, bank)
+function next_sub_Y = sum_Y(sub_Y, invariant)
+%% Cell-wise map
+if iscell(sub_Y)
+    sum_handle = @(x) sum_Y(x, invariant);
+    next_sub_Y = map_unary(sum_handle, sub_Y);
+    return
+end
 
 end
 
