7a6e0c24af2be615545efcd69cce6cab9bccea1a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-13 14:10:23 +0100

    add second input arg to map_collapse_ranges

--- lib/map/map_collapse_ranges.m
+++ lib/map/map_collapse_ranges.m
@@ -1,4 +1,4 @@
-function zeroth_ranges_out = map_collapse_ranges(zeroth_ranges_in)
+function zeroth_ranges_out = map_collapse_ranges(zeroth_ranges_in, subscripts)
 %% Cell-wise map
 if iscell(zeroth_range_in)
     cell_sizes = size(zeroth_ranges_in);
@@ -6,8 +6,9 @@ if iscell(zeroth_range_in)
     nCells = prod(cell_sizes);
     for cell_index = 1:nCells
         zeroth_ranges_out{cell_index} = ...
-            map_collapse_ranges(zeroth_ranges_in{cell_index});
+            map_collapse_ranges(zeroth_ranges_in{cell_index}, subscripts);
     end
+    return
 end
 
 end
