463f9327b04bc6491e929b9796cad89f29b3734b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-20 12:28:21 +0100

    remove key from metadata in sum_Y

--- lib/operators/sum_Y.m
+++ lib/operators/sum_Y.m
@@ -7,7 +7,8 @@ if iscell(sub_Y)
 end
 
 %% Get subscripts of summed variable
-leaf = get_leaf(sub_Y.variable_tree, invariant.behavior.key);
+key = invariant.behavior.key;
+leaf = get_leaf(sub_Y.variable_tree, key);
 subscripts = leaf.subscripts;
 
 %% Reduce all tensors along these subscripts
@@ -16,5 +17,11 @@ next_sub_Y.data = map_sum(sub_Y.data, subscripts);
 
 %% Update ranges
 next_sub_Y.ranges{1+0} = map_collapse_ranges(sub_Y.ranges{1+0}, subscripts);
+
+%% Remove key from variable tree
+next_sub_Y.variable_tree = remove_key(sub_Y.variable_tree, key);
+
+%% Remove key from jagged array
+next_sub_Y.keys{1+0}(subscripts) = [];
 end
 
