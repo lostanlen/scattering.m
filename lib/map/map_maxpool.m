f4abf858a7d2f5588e36a3100019362e95aae21b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-20 15:55:35 +0100

    write function max_maxpool (full pooling)

---
+++ lib/map/map_maxpool.m
@@ -0,0 +1,14 @@
+function data_out = map_maxpool(data_in, subscripts)
+%% Cell-wise map
+if iscell(data_in)
+    sizes = size(data_in);
+    data_out = cell(sizes);
+    nCells = prod(sizes);
+    for cell_index = 1:nCells
+        data_out{cell_index} = map_maxpool(data_in{cell_index}, subscripts);
+    end
+    return
+end
+
+%% Tail call
+data_out = max(data_in, [], subscripts);
