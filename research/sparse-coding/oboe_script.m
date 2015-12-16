54d325b67d257807817c28abf6e911b528408bdd
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-16 17:50:28 +0100

    start writing script

---
+++ research/sparse-coding/oboe_script.m
@@ -0,0 +1,17 @@
+% Order 1 along time
+opts{1}.time.T = 2048;
+opts{1}.time.max_scale = 4096; % about 93 ms
+opts{1}.time.max_Q = 8;
+ 
+% Nonlinearity between the two orders
+opts{1}.nonlinearity.name = 'modulus';
+ 
+% Order 2 in time
+opts{2}.time.handle = @gammatone_1d;
+
+%%
+archs = sc_setup(opts);
+
+%%
+oboe_path = 'research/sparse-coding/2366.wav';
+[waveform, sample_rate] = audioread_compat(oboe_script);
