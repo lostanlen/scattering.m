1c70813b57404d082ed32f0fc253568f784eed1d
siraferradans <sira.ferradans@gmail.com>
2015-12-17 17:38:56 +0100

    changes in sparse_backward

--- research/sparse-coding/learn_Dictionaries.m
+++ research/sparse-coding/learn_Dictionaries.m
@@ -1,4 +1,4 @@
-function [D,E]=learn_Dictionaries(Y,initnLambda,options)
+function [dict,E]=learn_Dictionaries(Y,initnLambda,options)
 % This function finds the dictioanries for each data set in the Y cell. We
 % assume the following dimensions:
 % * Y{lambda2}: Nxlambda1 where N is the number of exemplars and lambda1
@@ -10,14 +10,15 @@ function [D,E]=learn_Dictionaries(Y,initnLambda,options)
 
 L2 = length(Y);
 
-%options.learning_method = getoptions(options,'learning_method','ksvd');%algorithm
-%options.use_mex = 1;
-
+%Obtain dictionary backward: Y=D X
 for l=initnLambda:L2
     disp(['l=' num2str(l)]);
     [l1,N]=size(Y{l});
     options.K = round(l1/1.5); %we want less atoms than dimensions
    % [D{l},X,err] = perform_dictionary_learning(Y{l}+eps,options);
-    [D{l},X,err]=learn_dict(Y{l},options.K,N);
+    [dict.backward{l},X,err]=learn_dict(Y{l},options.K,N);
     E(l)=max(err);
+    
+    dict.forward{l} = pinv(dict.backward{l});
+    dict.forward_conjugate{l} = dict.backward{l}';
 end 
--- research/sparse-coding/sparse_backward.m
+++ research/sparse-coding/sparse_backward.m
@@ -1,10 +1,10 @@
-function Y = sparse_backward(X,dicts,initnLambda)
+function Y = sparse_backward(X,dicts)
 % Given a set of dictionaries (D) and the coefficients of a signal on this
 % dictioanry space (alpha), we reconstruct the original signal as Y=D alpha
 % Note that D=dicts{lambda2}, alpha = X{lambda2}
 
-L2 = length(X);
+nLambda2s = length(Y.data);
 
-for l=initnLambda:L2
-    Y{l} = dicts{l}*X{l};
+for l=dicts.lambda_start:nLambda2s
+    Y.data{l} = dicts.backward{l} *X.data{l};
 end
