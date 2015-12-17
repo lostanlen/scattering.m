09f0fa7baa1c3dcbcf3933c8f2c00f002cef6698
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-17 17:10:57 +0100

    next_sub_Y -> next_sub_Y.data in sparse_forward

--- research/sparse-coding/sparse_forward.m
+++ research/sparse-coding/sparse_forward.m
@@ -7,7 +7,7 @@ function next_sub_Y = sparse_forward(Y, dict)
 nLambda2s = length(Y.data);
 
 for lambda2_index = dict.lambda_start:L2
-    next_sub_Y{lambda2_index} = ...
+    next_sub_Y.data{lambda2_index} = ...
         dict.forward{lambda2_index} * Y.data{lambda2_index};
 end 
 end
