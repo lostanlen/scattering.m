6b250b76cb32450ff043177a05c96463cc2c7ac2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-18 13:05:22 +0100

    write saving loop in get_Y2s

--- research/sparse-coding/get_Y2s.m
+++ research/sparse-coding/get_Y2s.m
@@ -11,7 +11,7 @@ file_Y2s = cell(1,nFiles);
 %% Compute architectures
 archs = sc_setup(opts);
 
-%%
+%% Compute second-order scattering
 parfor file_index = 1:nFiles
     file_path = file_paths{file_index};
     waveform = audioread_compat(file_path);
@@ -20,4 +20,10 @@ parfor file_index = 1:nFiles
     file_Y2s{file_index} = Y2.data(lambda2_start:end);
     disp(['Finished file ', file_path, ' on worker ', labindex(), ...
         ' at ', datestr(now, 'HH:MM:SS')])
+end
+
+%% Save
+for file_index = 1:nFiles
+    file_Y2 = file_Y2s{file_index};
+    save(['Y2s_', num2str(file_index, '%0.2d')], 'file_Y2')
 end
