70338f909c6273340c4061ed6090377dde92b44e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-18 11:29:16 +0100

    write function get_Y2s

--- research/sparse-coding/get_Y2s.m
+++ research/sparse-coding/get_Y2s.m
@@ -1,4 +1,21 @@
 function Y2s = get_Y2s(dataset_path, opts)
+%% Get all waveforms
+file_paths = get_paths(dataset_path);
+nFiles = length(file_paths);
+file_Y2s = cell(1,nFiles);
+
+%% Compute architectures
+archs = sc_setup(opts);
+
+%%
+parfor file_index = 1:nFiles
+    file_path = file_paths{file_index};
+    waveform = audioread_compat(file_path);
+    [~,~,Y] = sc_propagate(waveform, archs);
+    file_Y2s{file_index} = unchunk_layer(Y{2}{end});
+    disp(['Finished file ', file_path, ' on worker ', labindex(), ...
+        ' at ', datestr(now, 'HH:MM:SS')])
+end
 
 end
 
