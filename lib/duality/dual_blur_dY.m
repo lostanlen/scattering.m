519265a29b408a5d7553e42dc3f01f49eb573433
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-22 09:21:59 +0100

    update dual_blur_dY with specific invariants

--- lib/duality/dual_blur_dY.m
+++ lib/duality/dual_blur_dY.m
@@ -1,30 +1,30 @@
-function previous_sub_dY = dual_blur_dY(sub_dY,bank)
+function previous_sub_dY = dual_blur_dY(sub_dY, invariant)
 %% Cell-wise map
 if iscell(sub_dY)
-    blur_handle = @(x) dual_blur_dY(x,bank);
-    previous_sub_dY = map_unary(blur_handle,sub_dY);
+    blur_handle = @(x) dual_blur_dY(x, invariant);
+    previous_sub_dY = map_unary(blur_handle, sub_dY);
     return
 end
 
 %% Variable loading
 keys = sub_dY.keys;
 variable_tree = sub_dY.variable_tree;
-sibling = get_relatives(bank.behavior.key,variable_tree);
-variable = get_leaf(variable_tree,bank.behavior.key);
+sibling = get_relatives(invariant.behavior.key, variable_tree);
+variable = get_leaf(variable_tree, invariant.behavior.key);
 
 % Subscripts and colons are updated according to the network structure
-bank.behavior.subscripts = variable.subscripts;
-bank.behavior.colons = substruct('()',replicate_colon(length(keys{1+0})));
+invariant.behavior.subscripts = variable.subscripts;
+invariant.behavior.colons = substruct('()', replicate_colon(length(keys{1+0})));
 
 %% Dual blurring
 if isempty(sibling)
     previous_sub_dY.data_ft = ...
-        dual_firstborn_blur(sub_dY.data,bank,sub_dY.ranges);
+        dual_firstborn_blur(sub_dY.data, invariant, sub_dY.ranges);
 elseif sibling.nSiblings==0
     previous_sub_dY.data_ft = ...
-        dual_secondborn_blur(sub_dY.data,bank,sub_dY.ranges,sibling);
+        dual_secondborn_blur(sub_dY.data, invariant, sub_dY.ranges, sibling);
 else
     previous_sub_dY.data_ft = ...
-        dual_sibling_blur(sub_dY.data,bank,sub_dY.ranges,sibling);
+        dual_sibling_blur(sub_dY.data, invariant, sub_dY.ranges, sibling);
 end
 end
