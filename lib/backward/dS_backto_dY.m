fcbb9d62926750175815d0e01f6893ff56aeb2b9
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-12-22 09:20:46 +0100

    backpropagate specific invariants in dS_backto_dY

--- lib/backward/dS_backto_dY.m
+++ lib/backward/dS_backto_dY.m
@@ -1,25 +1,25 @@
 function layer_dY_1 = dS_backto_dY(layer_dS,arch)
 %% Initialization
-nVariables_to_transform = length(arch.banks);
-banks = arch.banks;
+nVariables_to_transform = length(arch.invariants);
+invariants = arch.invariants;
 
 %% Backpropagate last variable
 start_index = nVariables_to_transform;
-while banks{start_index}.behavior.S.is_bypassed
+while invariants{start_index}.behavior.S.is_bypassed
     start_index = start_index - 1;
 end
-layer_dY{start_index} = dual_blur_dY(layer_dS,banks{start_index});
-layer_dY{start_index} = ...
-    perform_ift(layer_dY{start_index},banks{start_index}.behavior.subscripts);
+layer_dY{start_index} = dual_blur_dY(layer_dS,invariants{start_index});
+layer_dY{start_index} = perform_ift(layer_dY{start_index}, ...
+    invariants{start_index}.behavior.subscripts);
 
 %% Multi-variable case : iterated one-variable backpropagation
 % Wrapping the for loop inside an if statement yields an easy speed gain
 if start_index>1
     for variable_index = (start_index-1):-1:1
-        bank = banks{variable_index};
+        invariant = invariants{variable_index};
         layer_dY{variable_index} = ...
-            dual_blur_dY(layer_dY{variable_index+1},bank);
-        subscripts = bank.behavior.subscripts;
+            dual_blur_dY(layer_dY{variable_index+1},invariant);
+        subscripts = invariant.behavior.subscripts;
         layer_dY{variable_index} = ...
             perform_ift(layer_dY{variable_index},subscripts);
     end
