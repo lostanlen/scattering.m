d06a749af3b481b74ce9907a1f3c703b28a17751
siraferradans <sira.ferradans@gmail.com>
2015-12-28 11:01:49 +0100

    checking erros in test and training sets

--- research/sparse-coding/learn_oboeDB.m
+++ research/sparse-coding/learn_oboeDB.m
@@ -43,10 +43,17 @@ dicts.lambda_start = initnLambda;
 
 Y.data = subsetData;
 
-alphas = sparse_forward(Y, dicts);
+alphas = sparse_forward(Y, dicts); %Y-D*alphas
 Ytilde = sparse_backward(alphas, dicts);
 
 
+%% Now test on the test set
+for lambda2_index = initnLambda:length(Y.data)
+    Ytest.data{lambda2_index} = permute(Data{lambda2_index}(I(N+1:end),:),[2 1]);
+end 
+
+alphas_test = sparse_forward(Ytest, dicts); %Y-D*alphas
+Ytilde_test = sparse_backward(alphas_test, dicts);
 
 
 
@@ -54,17 +61,29 @@ Ytilde = sparse_backward(alphas, dicts);
 
 % check the overall error
 disp('Check the error in the approximation of the DB:')
+nLambda2s = length(Y.data);
 flat = @(x)x(:);
 errorComp = @(x)sum(x.^2,1);
+disp(['For training set: y-D alpha    y-y_tilde ']);
 for lambda2_index = initnLambda:nLambda2s
     
-    aux = dicts{lambda2_index}.backward*alpha.data{lambda2_index};
-    errorConstr=mean(errorComp(aux - Y2{lambda2_index}));
-    disp(['err on cosntr Y(' num2str(lambda2_index) ')=' num2str(errorConstr) ]);
+    aux = dicts.backward{lambda2_index}*alphas.data{lambda2_index};
+    errorConstr=norm(mean(errorComp(aux - Y.data{lambda2_index})));
+    errorY=norm(mean(errorComp(Ytilde.data{lambda2_index} - Y.data{lambda2_index})));
     
-    errorY=mean(errorComp(Ytilde.data{lambda2_index} - Y2{lambda2_index}));
-    disp(['err on Y(' num2str(lambda2_index) ')=' num2str(errorY) ]);
+    disp(['(' num2str(lambda2_index) ') ' num2str(errorConstr) '   ' num2str(errorY)]); 
+end  
+
+disp(['For test set: y-D alpha    y-y_tilde ']);
+for lambda2_index = initnLambda:nLambda2s 
+    
+    aux = dicts.backward{lambda2_index}*alphas_test.data{lambda2_index};
+    errorConstr=norm(mean(errorComp(aux - Ytest.data{lambda2_index})));
+    errorY=norm(mean(errorComp(Ytilde_test.data{lambda2_index} - Ytest.data{lambda2_index})));
+    disp(['(' num2str(lambda2_index) ') ' num2str(errorConstr) '   ' num2str(errorY)]);
+   
 end
+
 
 
 %% Show Y and DX for 7th lambda2
