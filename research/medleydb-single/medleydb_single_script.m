196fb498da00c35370020dc754a3f724c52b17f1
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-20 17:14:31 +0100

    update medleydb_single_script

--- research/medleydb-single/medleydb_single_script.m
+++ research/medleydb-single/medleydb_single_script.m
@@ -13,19 +13,28 @@ opts{2}.invariants.time.invariance = 'summed';
 archs = sc_setup(opts);
 
 %%
+S = sc_propagate(randn(N,1), archs);
+feature_vector = horzcat(format_layer(S{1+1}, 1), format_layer(S{1+2}, 1));
+nFeatures = length(feature_vector);
 
-
+%%
 training_paths = get_medleydb_paths(dataset_path, 'training');
-
 stem_paths = [training_paths{:}];
 chunk_paths = [stem_paths{:}];
-
 nSamples = length(chunk_paths);
 
-%for sample_index = 1:nSamples
-sample_index = 1; 
-chunk_path = chunk_paths{sample_index};
+X_train = zeros(nFeatures, nSamples);
 
-[stereo_waveform, sr] = audioread_compat(chunk_path);
-mono_waveform = mean(stereo_waveform, 2);
-S = sc_propagate(mono_waveform, archs);
+%%
+fprintf('Progress:\n');
+fprintf(['\n' repmat('.',1,m) '\n\n']);
+
+parfor sample_index = 1:nSamples
+    chunk_path = chunk_paths{sample_index};
+    stereo_waveform = audioread_compat(chunk_path);
+    mono_waveform = mean(stereo_waveform, 2);
+    S = sc_propagate(mono_waveform, archs);
+    X_train(:, sample_index) = ...
+        [format_layer(S{1+1}, 1), format_layer(S{1+2}, 1)];
+    fprintf('\b|\n');
+end
