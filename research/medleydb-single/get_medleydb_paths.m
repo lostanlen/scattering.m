9d35123b58c9311021c4369e5ed67ba045fa114a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-20 16:57:53 +0100

    write function get_medleydb_paths

---
+++ research/medleydb-single/get_medleydb_paths.m
@@ -0,0 +1,33 @@
+function paths = get_medleydb_paths(dataset_path, training_or_test)
+%%
+switch training_or_test
+    case 'training'
+        set_path = [dataset_path, '/', 'training'];
+    case 'test'
+        set_path = [dataset_path, '/', 'test'];
+end
+
+%%
+instrument_dirs = list_dir(set_path);
+instrument_names = {instrument_dirs.name};
+nInstruments = length(instrument_names);
+paths = cell(1, nInstruments);
+
+for instrument_index = 0:(nInstruments-1)
+    instrument_name = instrument_names{1+instrument_index};
+    instrument_path = [set_path, '/', instrument_name];
+    stem_dirs = list_dir(instrument_path);
+    stem_names = {stem_dirs.name};
+    nStems = length(stem_names);
+    paths{1+instrument_index} = cell(1, nStems);
+    for stem_index = 0:(nStems-1)
+        stem_name = stem_names{1+stem_index};
+        stem_path = [instrument_path, '/', stem_name];
+        chunk_dirs = list_dir(stem_path);
+        chunk_names = {chunk_dirs.name};
+        paths{1+instrument_index}{1+stem_index} = ...
+            cellfun(@(name) [stem_path, '/', name], chunk_names, ...
+            'UniformOutput', false);
+    end
+end
+end
