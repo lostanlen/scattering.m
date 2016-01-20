03d68b2ef38274bd340f3e557ae51c02e5676a50
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-01-20 17:04:19 +0100

    it works!

--- research/medleydb-single/medleydb_single_script.m
+++ research/medleydb-single/medleydb_single_script.m
@@ -7,14 +7,10 @@ opts{1}.banks.time.nFilters_per_octave = 12;
 opts{1}.banks.time.size = N;
 opts{1}.banks.time.T = 32768;
 opts{1}.invariants.time.invariance = 'summed';
-
-
-
-
+opts{2}.banks.time.nFilters_per_octave = 2;
+opts{2}.banks.gamma.nFilters_per_octave = 1;
+opts{2}.invariants.time.invariance = 'summed';
 archs = sc_setup(opts);
-
-%%
-S = sc_propagate(mono_waveform, archs);
 
 %%
 
