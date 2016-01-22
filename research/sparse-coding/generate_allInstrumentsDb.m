7b506960083ff882a39714dd3018df414ef554a4
siraferradans <sira.ferradans@gmail.com>
2016-01-22 15:01:54 +0100

    change into a function generate_allInstrumentsDB

--- research/sparse-coding/generate_allInstrumentsDb.m
+++ research/sparse-coding/generate_allInstrumentsDb.m
@@ -1,3 +1,5 @@
+function generate_allInstrumentsDb()
+
 %%% Addpath for Sira
 %sira_path = ...
 %    '/Users/ferradans/Documents/Research/AudioSynth/code/toolbox_sparsity/';
@@ -11,6 +13,7 @@ addpath(genpath(vincent_path));
 %% Setup options
 N = 131072;
 T = N / 4;
+initnLambda = 7;
 opts{1}.time.T = T;
 opts{1}.time.size = N;
 opts{1}.time.max_Q = 16;
@@ -23,16 +26,15 @@ opts{2}.time.T = T;
 opts{2}.time.max_scale = Inf;
 opts{2}.time.handle = @gammatone_1d;
 opts{2}.time.sibling_mask_factor = 2;
+opts{2}.time.gamma_bounds = [initnLambda Inf]
 opts{2}.time.max_Q = 1;
-
-nLambda2s = log2(T);
 
 %% Build filters
 archs = sc_setup(opts);
 
 %% Load database in order to compute the dictionaries
 disp('Load paths')
-dataset_path = '~/data/medleydb-single-instruments/'
+dataset_path = '~/data/medleydb-single-instruments/';
 training_paths = get_medleydb_paths(dataset_path, 'training');
 
 stem_paths = [training_paths{:}];
@@ -40,31 +42,31 @@ chunk_paths = [stem_paths{:}];
 
 %% initialize dataset 
 disp('initialize DB')
-[waveform, sample_rate] = audioread_compat(chunk_paths{1});
+[stereo_waveform, sample_rate] = audioread_compat(chunk_paths{1});
+mono_waveform = mean(stereo_waveform, 2);
 
-initnLambda = 7;
-[~,~,Yaux] = sc_propagate(waveform, archs);
-Y1 = unchunk_layer(Yaux{1}{end});
-nLambda1s = length(Y1.data);
+[~,~,Yaux] = sc_propagate(mono_waveform, archs);
 
-parfor lambda2_index = initnLambda:nLambda2s
+nLambda2s = length(Yaux{1+2}{end}.data);
+Y = cell(1, nLambda2s);
+parfor lambda2_index = 1:nLambda2s
+    nLambda1s = size(Yaux{1+2}{end}.data{lambda2_index}, 2);
     Y{lambda2_index} = zeros(nLambda1s,length(chunk_paths));  
 end 
 
 %% Compute scattering and save in DB
 disp('generate DB')
-for n=1:length(chunk_paths)
-    disp(['waveform:' num2str(n)]);
-    [waveform, sample_rate] = audioread_compat(chunk_paths{n});
-
+for n = 1:length(chunk_paths)
+    disp(['waveform: ' chunk_paths{n}]);
+    [stereo_waveform, sample_rate] = audioread_compat(chunk_paths{n});
+    mono_waveform = mean(stereo_waveform, 2);
+    
     %% Compute scattering
-    [~,~,Yaux] = sc_propagate(waveform, archs);
-
-    %%Unchunk Y2
-    Y2 = unchunk_layer(Yaux{2}{end});
+    [~,~,Y_sample] = sc_propagate(mono_waveform, archs);
+    Y2 = Y_sample{1+2}{end};
     nLambda2s = length(Y2.data);
-    for lambda2_index = initnLambda:nLambda2s
-        Y{lambda2_index}(:,n) = Y2.data(end/2,:);
+    for lambda2_index = 1:nLambda2s
+        Y{lambda2_index}(:,n) = Y2.data{lambda2_index}(end/2,:);
     end 
 end 
 disp('save DB')
