4ae83b5ad6682d99b589096d91ef03b65d9b4aa6
siraferradans <sira.ferradans@gmail.com>
2016-02-01 18:29:59 +0100

    load any DB

--- research/sparse-coding/main_generateDicts_DB.m
+++ research/sparse-coding/main_generateDicts_DB.m
@@ -1,14 +1,25 @@
-%load data set and learn the dictionaries
+disp('Generating data from all instruments')
+pathUrban = '~/data/UrbanSound8K/training/';
 
-%disp('Generating data from all instruments')
-%[Y,initnLambda]=generate_allInstrumentsDb();
-%disp('save DB')
+[Y,initnLambda]=generate_DB_frompath(pathLibri);
+disp('save DB')
+save('../../../../data/allUrban.mat','Y','initnLambda');
+
 %save('../../../../data/allInstrumentsDB_3secs.mat','Y','initnLambda');
-load('../../../../data/allInstrumentsDB_3secs.mat');
 %% Compute the dictionaries
 
-k_dim_coeff = 3;%percentage of dim that we want for the atoms of the dictionary
+k_dim_coeff = 1;%percentage of dim that we want for the atoms of the dictionary
 disp(['Learn the dictionaries:'])
-dicts = learn_Dictionaries(Y,6,k_dim_coeff);
-save('../../../../data/Dictionary_overcomplete.mat','dicts');
-disp('.. and saving dictionaries ')
+dicts = learn_Dictionaries(Y,1,k_dim_coeff);
+save('../../../../data/Dictionary_Urban.mat',dicts);
+
+returnl 
+
+
+
+d = dir('./training/')
+P = randperm(size(d,1));
+
+for i=1:3000
+    system(['mv ./training/' d(P(i)).name ' ./testing/.' ]);
+end 
