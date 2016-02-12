c34951aecc8c0ebe2aa93498e35ce0aee5e437bd
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-12 21:09:29 +0100

    bis

--- research/medleydb-single/classwise_accuracies.m
+++ research/medleydb-single/classwise_accuracies.m
@@ -1,5 +1,5 @@
 function [accuracies, cm] = classwise_accuracies(Y_predicted, Y_true)
-n_classes = max(Y_true);
+n_classes = 1 + max(Y_true);
 cm = zeros(n_classes, n_classes);
 for sample_index = 1:length(Y_predicted)
     cm(1 + Y_predicted(sample_index), 1 + Y_true(sample_index)) = ...
