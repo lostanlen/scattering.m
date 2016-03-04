747521c9512e90edb219c430d7a1a5015d7e0aff
siraferradans <sira.ferradans@gmail.com>
2016-03-04 09:31:40 +0100

    typo and size control

--- lib/indexing/fetch_firstorder.m
+++ lib/indexing/fetch_firstorder.m
@@ -1,5 +1,5 @@
 function path = fetch_firstorder(S1, gamma1)
 gamma1_start = S1.ranges{1}(1,2);
-path = S1.data(:, gamma1 - gamma_start + 1);
+path = S1.data(:, gamma1 - gamma1_start + 1);
 end
 
--- lib/indexing/fetch_plain.m
+++ lib/indexing/fetch_plain.m
@@ -1,7 +1,18 @@
 function path =  fetch_plain(S2, gamma1, gamma2)
-gamma2_start = S2.ranges{1+1}(1,1);
-gamma2_index = gamma2 - gamma2_start + 1;
-gamma1_start = S2.ranges{1+0}{gamma2_index}(1,2);
-gamma1_index = gamma1 - gamma1_start + 1;
-path = S2.data{gamma2_index}(:, gamma1_index);
+    gamma2_start = S2.ranges{1+1}(1,1);
+    gamma2_index = gamma2 - gamma2_start + 1;
+    
+    if ( (gamma2_index>length(S2.ranges{1+0})) || (gamma2_index < 1))
+        path=0;
+    else 
+        gamma1_start = S2.ranges{1+0}{gamma2_index}(1,2);
+        gamma1_index = gamma1 - gamma1_start + 1;
+        
+        if ( (gamma1_index > size(S2.data{gamma2_index},2)) || ...
+             (gamma1_index < 1) )
+         path= 0; 
+        else 
+            path = S2.data{gamma2_index}(:, gamma1_index);
+        end 
+    end 
 end
