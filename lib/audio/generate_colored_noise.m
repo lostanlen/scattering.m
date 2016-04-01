690b3a158beebcef969f1a11e5730e29b1b47ee7
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-01 16:59:27 +0200

    update generate_colored_noise

--- lib/audio/generate_colored_noise.m
+++ lib/audio/generate_colored_noise.m
@@ -1,12 +1,15 @@
 function colored_noise = generate_colored_noise(signal)
-esignal_dimension = sum(size(signal)~=1);
+signal_dimension = sum(size(signal)~=1);
 switch signal_dimension
     case 1
         N = size(signal, 1);
         % Draw phases uniformly at random
-        phases = 2 * pi * rand(N, 1);
-        spectrum = abs(fft(signal));
-        colored_noise = ifft(phases .* spectrum)
+        phasors = exp(2i * pi * rand(N/2 - 1, 1));
+        colored_noise_ft = abs(fft(signal));
+        colored_noise_ft(2:(end/2)) = colored_noise_ft(2:(end/2)) .* phasors;
+        colored_noise_ft(end:(-1):(end/2+2)) = ...
+            colored_noise_ft(end:(-1):(end/2+2)) .* conj(phasors);
+        colored_noise = ifft(colored_noise_ft);
     case 2
         error('Colored noise generation not ready in dimension 2');
 end
