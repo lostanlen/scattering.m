0a87a59459c99894f68d4ba8fe55e26875099acb
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-05 14:47:47 +0200

    remove biodiversity and sparse-coding folders

--- research/medleydb-single/medleydb_script.m
+++ research/medleydb-single/medleydb_script.m
@@ -1,5 +1,6 @@
 dataset_path = '~/datasets/medleydb-single-instruments';
 method = 'plain';
+
 %%
 N = 131072;
 octave_bounds = [2 8];
@@ -22,40 +23,42 @@ end
 opts{2}.banks.wavelet_handle = @gammatone_1d;
 opts{2}.invariants.time.invariance = 'summed';
 
-%% Create architecture
-archs = sc_setup(opts);
+file_name = ['mdb', method];
+var_name = ['mdb', method, '_data'];
+
+is_memoized = true;
+if ~is_memoized
+    archs = sc_setup(opts);
+    
+    [X_training, X_test] = get_medleydb_features(archs);
+    [Y_training, Y_test] = get_medleydb_labels();
+    
+    data.X_training = X_training;
+    data.X_test = X_test;
+    data.Y_training = Y_training;
+    data.Y_test = Y_test;
+    data.opts = opts;
+    eval([var_name, ' = data']);
+    save(file_name, var_name);
+else
+    load(file_name, var_name);
+    eval(['data = ', var_name]);
+end
+
+
+%% Standardize features
+X_mean = mean(X_training, 2);
+X_std = std(X_training, 2);
+X_training_centered = bsxfun(@minus, X_training, X_mean);
+X_training_std = bsxfun(@rdivide, X_training, X_std);
+X_test_centered = bsxfun(@minus, X_training, X_mean);
+X_test_std = bsxfun(@rdivide, X_test, X_std);
 
 %%
-[X_training, X_test] = get_medleydb_features(archs);
-[Y_training, Y_test] = get_medleydb_labels();
-
-%%
-data.X_training = X_training;
-data.X_test = X_test;
-data.Y_training = Y_training;
-data.Y_test = Y_test;
-data.opts = opts;
-
-data_name = ['mdb', method, '_data'];
-eval([data_name, ' = data']);
-save(['mdb', data_name], data);
-
-%%
-classifier_options = statset('UseParallel', true);
-NumTrees = 100;
 [nSamples_per_class, ~] = hist(Y_training, 0:7);
 class_frequencies = nSamples_per_class / sum(nSamples_per_class);
 class_weights = 1 ./ class_frequencies;
 cost_matrix = repmat(class_weights.', 1, 8) - diag(class_weights);
-%%
 
-B = TreeBagger(NumTrees, ...
-    X_training, ...
-    Y_training, ...
-    'Cost', cost_matrix, ...
-    'Options', classifier_options);
-
-Y_predicted = predict(B, X_test);
-Y_predicted = cellfun(@str2double, Y_predicted);
 %%
-accuracies = classwise_accuracies(Y_predicted, Y_test)
+model = svmtrain(Y_train, X_train);
