26428f252c7a01c8a57ea1e5c15657102ea61e67
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-07 13:04:45 +0200

    Morlet wavelets, variable N in eca_setup

--- research/eca/eca_setup.m
+++ research/eca/eca_setup.m
@@ -1,13 +1,11 @@
-function archs = eca_setup(Q1, T, modulations)
+function archs = eca_setup(N, Q1, T, modulations)
+% N is the length of the input, it should be a power of 2.
 % Q1 is the number of filters per octave
 % T is the amount of invariance with respect to temporal translation
 % modulation is a string which can either be set to
 % 1. 'none' for averaged spectrum only
 % 2. 'time' for temporal scattering
 % 3. 'time-frequency' for time-frequency scattering
-
-% N is the length of the input
-N = 131072; % about 3 s at 44,1 kHz
 
 % Options for spectrogram
 opts{1}.time.nFilters_per_octave = Q1;
@@ -20,6 +18,7 @@ opts{1}.time.duality = 'hermitian';
 if strcmp(modulations, 'time') || strcmp(modulations, 'time-frequency')
     % Options for temporal modulations
     opts{2}.time.nFilters_per_octave = 1;
+    opts{2}.time.wavelet_handle = @morlet_1d;
     opts{2}.time.duality = 'hermitian';
 end
     
