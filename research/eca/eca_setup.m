023fdbf3e30190bc3befd62c4aaf226bc732317d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-09 19:07:35 +0200

    9 octaves of auditory range

--- research/eca/eca_setup.m
+++ research/eca/eca_setup.m
@@ -12,7 +12,7 @@ opts{1}.time.nFilters_per_octave = Q1;
 opts{1}.time.T = T;
 opts{1}.time.size = N;
 opts{1}.time.is_chunked = false;
-opts{1}.time.gamma_bounds = [1 Q1*8];
+opts{1}.time.gamma_bounds = [1 Q1*9];
 opts{1}.time.duality = 'hermitian';
 
 if strcmp(modulations, 'time') || strcmp(modulations, 'time-frequency')
