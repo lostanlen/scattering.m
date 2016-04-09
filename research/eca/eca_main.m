346c84c73dd4bf484600cfc1ae10af4cf078c4cd
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-09 19:08:05 +0200

    eca_main

--- research/eca/eca_main.m
+++ research/eca/eca_main.m
@@ -1,2 +1,26 @@
+%% Setup
+N = 2^17; % 2^17 = 131072, about 3 seconds.
+Q1 = 8; % number of filters per octave at first order
+T = 2^15; % amount of invariance with respect to time translation
+% The modulation setting is either 'none', 'time', or 'time-frequency'
+modulations = 'time-frequency';
+archs = eca_setup(N, Q1, T, modulations);
+
+%% Load
 audio_path = 'research/gretsi15/Vc-scale-chr-asc.wav';
 [y, sample_rate, bit_depth] = eca_load(audio_path);
+eca_display(y, archs);
+
+%% Re-synthesize
+opts.is_sonified = true;
+% (close Figure 1 to abort early)
+opts.nIterations = 50;
+iterations = eca_synthesize(y, archs, opts);
+
+%% Export
+export_mode = 'all'; % can be 'last' or 'all'
+eca_export(iterations, audio_path, export_mode, sample_rate, bit_depth, ...
+    Q1, T, modulations);
+
+%% Clear (run only if necessary)
+eca_clear(audio_path, Q1, T, modulations);
