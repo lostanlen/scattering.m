24693a39815674f08690f34c3d4022899269b6e2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-09 19:06:07 +0200

    write function eca_load

---
+++ research/eca/eca_load.m
@@ -0,0 +1,19 @@
+function [y, sample_rate, bit_depth] = eca_load(audio_path)
+[y, sample_rate] = audioread(audio_path, 'native');
+switch class(y)
+    case 'int16'
+        bit_depth = 16;
+        y = double(y) / 2^16;
+    case 'int32'
+        bit_depth = 24;
+        y = double(y) / 2^32;
+end
+if size(y, 1) == 1 && size(y, 2) > 1
+     y = y.';
+end
+if length(y) < N
+    y = cat(1, y, zeros(N - length(y)));
+else
+    y = y(1:N);
+end
+end
