58d817aa1ec7d8b60a2473fed4db921ea9723141
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-27 22:10:24 +0200

    update get_medleydb_labels

--- research/medleydb-single/get_medleydb_labels.m
+++ research/medleydb-single/get_medleydb_labels.m
@@ -1,33 +1,31 @@
-function [Y_training, Y_test] = get_medleydb_labels(dataset_path)
+function [Y_training, Y_validation, Y_test] = get_medleydb_labels(dataset_path)
 if nargin<1
     dataset_path = '~/datasets/medleydb-single-instruments';
 end
 
-%% Get training set labels
-training_paths = get_medleydb_paths(dataset_path, 'training');
-training_paths = [training_paths{:}];
-training_paths = [training_paths{:}];
-nSamples = length(training_paths);
-Y_training = zeros(nSamples, 1);
-for sample_index = 1:nSamples
-    sample_path = training_paths{sample_index};
-    [file_path, ~] = fileparts(sample_path);
-    [instrument_path, ~] = fileparts(file_path);
-    [~, instrument_name] = fileparts(instrument_path);
-    Y_training(sample_index) = str2double(instrument_name(1:2));
+%% Initialize
+subfolders = {'training', 'validation', 'test'};
+nSubfolders = length(subfolders);
+Ys = cell(1, nSubfolders);
+
+%% Get labels
+for subfolder_index = 1:nSubfolders
+    paths = get_medleydb_paths(dataset_path, 'training');
+    paths = [paths{:}];
+    paths = [paths{:}];
+    nSamples = length(paths);
+    Y = zeros(nSamples, 1);
+    for sample_index = 1:nSamples
+        sample_path = paths{sample_index};
+        [file_path, ~] = fileparts(sample_path);
+        [instrument_path, ~] = fileparts(file_path);
+        [~, instrument_name] = fileparts(instrument_path);
+        Y(sample_index) = str2double(instrument_name(1:2));
+    end
+    Ys{subfolder_index} = Y;
 end
 
-%% Get test set labels
-test_paths = get_medleydb_paths(dataset_path, 'test');
-test_paths = [test_paths{:}];
-test_paths = [test_paths{:}];
-nSamples = length(test_paths);
-Y_test = zeros(nSamples, 1);
-for sample_index = 1:nSamples
-    sample_path = test_paths{sample_index};
-    [file_path, ~] = fileparts(sample_path);
-    [instrument_path, ~] = fileparts(file_path);
-    [~, instrument_name] = fileparts(instrument_path);
-    Y_test(sample_index) = str2double(instrument_name(1:2));
-end
+Y_training = Ys{1};
+Y_validation = Ys{2};
+Y_test = Ys{3};
 end
