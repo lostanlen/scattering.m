3acd82782a793f9f2703cf46f284c8bf5c7b70f7
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-27 19:02:09 +0200

    bug fix eca_overlap_add

--- research/eca/eca_overlap_add.m
+++ research/eca/eca_overlap_add.m
@@ -2,7 +2,7 @@ function y = eca_overlap_add(chunks)
 %% Initialize y with zero
 [N, nChunks] = size(chunks);
 hop_length = N/2;
-y_length = hop_length * nChunks;
+y_length = hop_length * (nChunks - 1);
 y = zeros(y_length, 1);
 
 % Case of first chunk
@@ -10,7 +10,7 @@ y(1:hop_length) = chunks((1+hop_length):end, 1);
 
 % General case
 for chunk_index = 2:(nChunks-1)
-    chunk_start = (chunk_index-1) * hop_length + 1;
+    chunk_start = (chunk_index-2) * hop_length + 1;
     chunk_stop = chunk_start + 2 * hop_length - 1;
     y(chunk_start:chunk_stop) = ...
         y(chunk_start:chunk_stop) + ...
