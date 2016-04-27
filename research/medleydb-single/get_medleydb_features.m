94604f338900bb90b71e1203b8a2411bc6b5f492
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-04-27 22:10:34 +0200

    update get_medleydb_features

--- research/medleydb-single/get_medleydb_features.m
+++ research/medleydb-single/get_medleydb_features.m
@@ -1,4 +1,5 @@
-function [X_train, X_test] = get_medleydb_features(archs, dataset_path)
+function [X_training, X_validation, X_test] = ...
+    get_medleydb_features(archs, dataset_path)
 if nargin<2
     dataset_path = '~/datasets/medleydb-single-instruments';
 end
@@ -9,40 +10,33 @@ S = sc_propagate(randn(N,1), archs);
 feature_vector = horzcat(format_layer(S{1+1}, 1), format_layer(S{1+2}, 1));
 nFeatures = length(feature_vector);
 
-%% Get training set features
-training_paths = get_medleydb_paths(dataset_path, 'training');
-stem_paths = [training_paths{:}];
-chunk_paths = [stem_paths{:}];
-nSamples = length(chunk_paths);
-X_train = zeros(nSamples, nFeatures);
-parfor_progress(nSamples);
-parfor sample_index = 1:nSamples
-    chunk_path = chunk_paths{sample_index};
-    stereo_waveform = audioread_compat(chunk_path);
-    mono_waveform = mean(stereo_waveform, 2);
-    S = sc_propagate(mono_waveform, archs);
-    X_train(sample_index, :) = ...
-        [format_layer(S{1+1}, 1), format_layer(S{1+2}, 1)].';
-    parfor_progress();
-end
-parfor_progress(0);
+%% Get features
+subfolders = {'training', 'validation', 'test'};
+nSubfolders = length(subfolders);
+Xs = cell(1, nSubfolders);
 
-%% Get test set features
-test_paths = get_medleydb_paths(dataset_path, 'test');
-stem_paths = [test_paths{:}];
-chunk_paths = [stem_paths{:}];
-nSamples = length(chunk_paths);
-X_test = zeros(nSamples, nFeatures);
-parfor_progress(nSamples);
-parfor sample_index = 1:nSamples
-    chunk_path = chunk_paths{sample_index};
-    stereo_waveform = audioread_compat(chunk_path);
-    mono_waveform = mean(stereo_waveform, 2);
-    S = sc_propagate(mono_waveform, archs);
-    X_test(sample_index, :) = ...
-        [format_layer(S{1+1}, 1), format_layer(S{1+2}, 1)].';
-    parfor_progress();
+for subfolder_index = 1:nSubfolders
+    paths = get_medleydb_paths(dataset_path, 'training');
+    stem_paths = [paths{:}];
+    chunk_paths = [stem_paths{:}];
+    nSamples = length(chunk_paths);
+    X = zeros(nSamples, nFeatures);
+    parfor_progress(nSamples);
+    parfor sample_index = 1:nSamples
+        chunk_path = chunk_paths{sample_index};
+        stereo_waveform = audioread_compat(chunk_path);
+        mono_waveform = mean(stereo_waveform, 2);
+        S = sc_propagate(mono_waveform, archs);
+        X(sample_index, :) = ...
+            [format_layer(S{1+1}, 1), format_layer(S{1+2}, 1)].';
+        parfor_progress();
+    end
+    parfor_progress(0);
+    Xs{subfolder_index} = X;
 end
-parfor_progress(0);
+
+X_training = Xs{1};
+X_validation = Xs{2};
+X_test = Xs{3};
 end
 
