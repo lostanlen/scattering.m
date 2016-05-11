9147371182207701e1a2c2e619ae5767a33d4a87
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-05-11 15:56:34 +0200

    update medleydb_classify

--- research/medleydb-single/medleydb_classify.py
+++ research/medleydb-single/medleydb_classify.py
@@ -9,7 +9,7 @@ import sklearn.preprocessing
 
 np.set_printoptions(precision=2)
 
-method = 'plain'
+method = 'joint'
 
 # Load
 mat = scipy.io.loadmat("mdb" + method + ".mat")
@@ -26,37 +26,65 @@ Y_test = np.ravel(data["Y_test"][0][0])
 X_training = np.concatenate((X_training, X_validation))
 Y_training = np.concatenate((Y_training, Y_validation))
 
-# Discard features with less than 1% of the variance
-# Without it, performance drops from 81% to 65%
-# variances = np.var(X_training, axis=0)
-# variances /= np.sum(variances)
-# negative_variances = - variances
-# sorting_indices = np.argsort(negative_variances)
-# sorted_variances = variances[sorting_indices]
-# cumulative_variances = np.cumsum(sorted_variances)
-#
-# n_features = np.where(cumulative_variances > 0.99)[0][0]
-# dominant_indices = sorting_indices[:n_features]
-#
-# X_training = X_training[:, dominant_indices]
-# X_validation = X_validation[:, dominant_indices]
-# X_test = X_test[:, dominant_indices]
+# Discard features with less than 5% of the energy
+energies = X_training * X_training
+feature_energies = np.mean(X_training, axis=0)
+feature_energies /= np.sum(feature_energies)
+sorting_indices = np.argsort(feature_energies)
+sorted_feature_energies = feature_energies[sorting_indices]
+cumulative = np.cumsum(sorted_feature_energies)
+start_feature = np.where(cumulative > 0.1)[0][0]
+dominant_indices = sorting_indices[start_feature:]
 
-# Logarithmic transformation
-# Without it, performance drops from 81% to 69%
-medians = np.median(X_training, axis=0)[np.newaxis, :]
-log1p_denominators = 1e-2 * medians
-X_training = np.log1p(X_training / log1p_denominators)
-X_test = np.log1p(X_test / log1p_denominators)
+X_training = X_training[:, dominant_indices]
+X_validation = X_validation[:, dominant_indices]
+X_test = X_test[:, dominant_indices]
 
+n_examples, n_features = X_training.shape
+toleration = 1e-3
+den = np.zeros(n_features)
+for feature_id in range(n_features):
+    feature = X_training[:, feature_id]
+    lower_bound = np.finfo(float).eps
+    if scipy.stats.skew(np.log(lower_bound + feature)) > 0.0:
+        X_training[:, feature_id] = np.log(lower_bound + feature)
+        X_test[:, feature_id] = np.log(lower_bound + X_test[:, feature_id])
+    else:
+        upper_bound = np.median(feature)
+        midpoint = 0.5 * (lower_bound + upper_bound)
+        skewness = scipy.stats.skew(np.log1p(feature / midpoint))
+        while abs(skewness) > toleration:
+            if skewness > 0.0:
+                upper_bound = midpoint
+            elif skewness < 0.0:
+                lower_bound = midpoint
+            midpoint = 0.5 * (lower_bound + upper_bound)
+            skewness = scipy.stats.skew(np.log1p(feature / midpoint))
+        den[feature_id] = midpoint / np.median(feature)
+        X_training[:, feature_id] = np.log1p(feature / midpoint)
+        X_test[:, feature_id] = np.log1p(X_test[:, feature_id] / midpoint)
+
+
+# PCA
+centerer = sklearn.preprocessing.StandardScaler(with_std=False)
+centerer.fit(X_training)
+X_training = centerer.transform(X_training)
+X_test = centerer.transform(X_test)
+pca = sklearn.decomposition.PCA()
+X_training = pca.fit_transform(X_training)
+X_test = pca.transform(X_test)
+Xtr = X_training
+Xte = X_test
+
+
+X_training = Xtr[:, :20]
+X_test = Xte[:, :20]
 # Standardize features
-# With it, performance goes from 81% to 82%
 scaler = sklearn.preprocessing.StandardScaler().fit(X_training)
 X_training = scaler.transform(X_training)
-X_validation = scaler.transform(X_validation)
 X_test = scaler.transform(X_test)
 
-Cs = [1e2]
+Cs = [1e5]
 report = []
 output_file = open('mdb' + method + 'svm_y.pkl', 'wb')
 pickle.dump(report, output_file)
