ad27dfa21699ba9cc24f758836d0f13a2dd7d738
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-08-05 04:20:10 +0200

    update main script

--- research/eca/eca_main.m
+++ research/eca/eca_main.m
@@ -1,10 +1,11 @@
 %% Setup
+addpath(genpath('../../'))
 Q1 = 12; % number of filters per octave at first order
-T = 2^10; % amount of invariance with respect to time translation
+T = 2^13; % amount of invariance with respect to time translation
 % The modulation setting is either 'none', 'time', or 'time-frequency'
 % The wavelets setting is either 'morlet' or 'gammatone'
 modulations = 'time-frequency';
-wavelets = 'gammatone';
+wavelets = 'morlet';
 archs = eca_setup(Q1, T, modulations, wavelets);
 
 %% Load
@@ -12,20 +13,19 @@ audio_path = '/Users/vlostan/datasets/eca/modulator_7m04s.wav';
 [y, sample_rate, bit_depth] = eca_load(audio_path);
 eca_display(y, archs);
 
-%%
-[text, S_sorted_paths] = eca_text(y, archs, sample_rate);
-disp(text)
-
 %% Re-synthesize
-opts.is_sonified = true;
+opts.is_sonified = false;
 % (close Figure 1 to abort early)
 opts.nIterations = 50;
-iterations = eca_synthesize(y, archs, opts);
+opts.sample_rate = sample_rate; 
+opts.generate_text = true;
+[sounds, texts] = eca_synthesize(y, archs, opts);
 
-%% Export
+%% Export sounds and text
 opts.export_mode = 'all'; % can be 'last' or 'all'
-eca_export(iterations, audio_path, opts, sample_rate, bit_depth, ...
+eca_export_sounds(sounds, audio_path, opts, sample_rate, bit_depth, ...
     Q1, T, modulations);
+eca_export_texts(texts, audio_path, opts, Q1, T, modulations);
 
 %% Clear (run only if necessary)
 eca_clear(audio_path, Q1, T, modulations);
