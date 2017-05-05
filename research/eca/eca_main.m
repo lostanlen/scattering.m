b4278207284548a6056a3fba63e677d2268b6ef7
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-05-05 16:44:38 -0400

    up  eca_main

--- research/eca/eca_main.m
+++ research/eca/eca_main.m
@@ -1,31 +1,33 @@
+addpath(genpath('../..'));
+
 %% Setup scattering options
 Q1 = 12; % number of filters per octave at first order
-T = 2^8; % amount of invariance with respect to time translation
+T = 2^10; % amount of invariance with respect to time translation
 % The modulation setting is either 'none', 'time', or 'time-frequency'
 % The wavelets setting is either 'morlet' or 'gammatone'
-modulations = 'none';
+modulations = 'time-frequency';
 wavelets = 'morlet';
-N = 2^17; % length of the signal
+N = 2^16; % length of the signal
 
 % Load
-audio_path = '~/datasets/solosDb/Pn/2985.wav';
+audio_path = '~/datasets/eca/bach.wav';
 [y, sample_rate, bit_depth] = eca_load(audio_path, N);
 
 %% Setup reconstruction options
 clear opts;
 opts.nChunks_per_batch = 2; % must be > 1
-opts.is_sonified = true;
-opts.is_spectrogram_displayed = false;
+opts.is_sonified = false;
+opts.is_spectrogram_displayed = true;
 % (close Figure 1 to abort early)
-opts.nIterations = 20;
+opts.nIterations = 50;
 opts.sample_rate = sample_rate;
 opts.generate_text = false;
-opts.is_verbose = true;
+opts.is_verbose = false;
 opts.initial_learning_rate = 0.1;
 
 %% Multi-chunk mode
 archs_multichunk = eca_setup(Q1, T, modulations, wavelets);
-chunk_sounds = eca_synthesize(y, archs_multichunk, opts);
+sounds_multichunk = eca_synthesize(y, archs_multichunk, opts);
 
 %% Single-chunk mode
 archs_singlechunk = eca_setup_1chunk(Q1, T, modulations, wavelets, N);
