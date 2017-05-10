da18410df7440a4eed09a50097e1090ae5f2c1d7
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-05-10 14:42:27 -0400

    update generate_text

--- research/eca/generate_text.m
+++ research/eca/generate_text.m
@@ -1,6 +1,6 @@
 % Replace these paths
 toolbox_path = '~/scattering.m';
-audio_path = '~/datasets/eca/bach1.wav';
+folder = '~/datasets/eca';
 
 addpath(genpath(toolbox_path));
 
@@ -12,16 +12,9 @@ T = 2^10; % amount of invariance with respect to time translation
 modulations = 'time-frequency';
 wavelets = 'morlet';
 archs = eca_setup(Q1, T, modulations, wavelets);
-N = archs{1}.banks{1}.spec.size;
 
 %%
 % set to Inf to get all lines
 % set to NaN ("not a number") to get all lines with >0 ppm
 nLines = Inf; 
-[y, sample_rate] = eca_load(audio_path);
-padding_length = ceil(length(y)/N) * N - length(y);
-y = cat(1, y, zeros(padding_length, 1));
-y_chunks = eca_split(y, N);
-S = sc_propagate(y_chunks, archs);
-[text, S_sorted_paths] = eca_text(S, nLines, sample_rate);
-disp(text)
+eca_text_dir(archs, folder, nLines)
