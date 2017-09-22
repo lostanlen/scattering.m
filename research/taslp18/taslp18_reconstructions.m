279c98ef1a2dba890eff343e06b7dfc386bcdcf3
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-09-22 15:36:08 -0400

    update taslp18_reconstructions.m

--- research/taslp18/taslp18_reconstructions.m
+++ research/taslp18/taslp18_reconstructions.m
@@ -1,5 +1,27 @@
+% Define parameters.
 Q1 = 16;
 T = 32;
 modulations = 'none';
 wavelets = 'morlet';
 N = 131072;
+
+
+% Load waveform.
+audio_path = 'taslp18_flute.wav';
+[target_waveform, sample_rate] = taslp18_load(audio_path, N);
+
+
+% Construct wavelet filter banks.
+archs = taslp18_setup(Q1, T, modulations, wavelets, N);
+
+
+% Compute scattering transform.
+[tagret_S, target_U] = sc_propagate(target_waveform, sample_rate);
+
+
+subplot(212);
+target_U = sc_unchunk(U(1:2));
+target_scalogram = display_scalogram(U{1+1});
+imagesc(log1p(target_scalogram./10.0));
+colormap rev_magma;
+drawnow();
