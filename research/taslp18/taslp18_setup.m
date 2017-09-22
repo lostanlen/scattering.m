6022b394402c2e52eb3fb1347e98be790b1debd2
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-09-22 16:14:49 -0400

    update taslp18_setup with custom T

--- research/taslp18/taslp18_setup.m
+++ research/taslp18/taslp18_setup.m
@@ -15,12 +15,11 @@ function archs = taslp18_setup(Q1, T, modulations, wavelets, N)
 % It should be a power of 2.
 
 opts{1}.time.nFilters_per_octave = Q1;
-opts{1}.time.T = T;
+opts{1}.time.T = 2^8;
 opts{1}.time.max_scale = 8192;
 opts{1}.time.size = N;
 opts{1}.time.is_chunked = false;
-opts{1}.time.nOctaves = 9;
-opts{1}.time.gamma_bounds = [1 Q1*9];
+opts{1}.time.gamma_bounds = [1 Q1*8];
 opts{1}.time.duality = 'hermitian';
 switch wavelets
     case 'morlet'
@@ -30,16 +29,21 @@ switch wavelets
     otherwise
         error(['Unrecognized field wavelets: ', wavelets]);
 end
+
+
+% Define options for low-pass filtering of time-frequency representation.
+opts{2}.invariants.time.T = T;
+
 if strcmp(modulations, 'time') || strcmp(modulations, 'time-frequency')
     % Options for temporal modulations
-    opts{2}.time.nFilters_per_octave = 1;
-    opts{2}.time.wavelet_handle = @morlet_1d;
-    opts{2}.time.duality = 'hermitian';
+    opts{2}.banks.time.nFilters_per_octave = 1;
+    opts{2}.banks.time.wavelet_handle = @morlet_1d;
+    opts{2}.banks.time.duality = 'hermitian';
     switch wavelets
         case 'morlet'
-            opts{2}.time.wavelet_handle = @morlet_1d;
+            opts{2}.banks.time.wavelet_handle = @morlet_1d;
         case 'gammatone'
-            opts{2}.time.wavelet_handle = @gammatone_1d;
+            opts{2}.banks.time.wavelet_handle = @gammatone_1d;
         otherwise
             error(['Unrecognized field wavelets: ', wavelets]);
     end
@@ -48,9 +52,9 @@ elseif ~strcmp(modulations, 'none')
 end
 % Options for frequential modulations
 if strcmp(modulations, 'time-frequency')
-    opts{2}.gamma.duality = 'hermitian';
-    opts{2}.gamma.nFilters_per_octave = 1;
-    opts{2}.gamma.subscripts = 2;
+    opts{2}.banks.gamma.duality = 'hermitian';
+    opts{2}.banks.gamma.nFilters_per_octave = 1;
+    opts{2}.banks.gamma.subscripts = 2;
 end
 archs = sc_setup(opts);
 end
