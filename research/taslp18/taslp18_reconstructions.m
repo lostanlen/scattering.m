70330b85863149ccdbfc8814e4d3fc10c37291db
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-09-27 15:12:28 -0400

    update taslp18_reconstructions

--- research/taslp18/taslp18_reconstructions.m
+++ research/taslp18/taslp18_reconstructions.m
@@ -7,26 +7,27 @@ N = 131072;
 
 
 % Load waveform.
-%audio_path = 'taslp18_flute.wav';
-audio_path = 'taslp18_dog-bark.wav';
-[target_waveform, sample_rate] = taslp18_load(audio_path, N);
+audio_names = {'taslp18_dog-bark.wav', 'taslp18_flute.wav'};
 
+for audio_name_id = 1:length(audio_names)
+    audio_name = audio_names{audio_name_id};
+    audio_path = [audio_name, '.wav'];
 
-% Construct wavelet filter banks.
-archs = taslp18_setup_visualization(Q1, N);
+    [target_waveform, sample_rate] = taslp18_load(audio_path, N);
 
+    % Construct wavelet filter banks.
+    archs = taslp18_setup_visualization(Q1, N);
 
-% Compute scattering transform.
-[target_S, target_U] = sc_propagate(target_waveform, archs);
+    % Compute scattering transform.
+    [target_S, target_U] = sc_propagate(target_waveform, archs);
 
+    target_scalogram = display_scalogram(target_U{1+1});
+    imagesc(log1p(target_scalogram./10.0));
+    colormap rev_magma;
+    axis off;
+    drawnow();
 
-target_scalogram = display_scalogram(target_U{1+1});
-imagesc(log1p(target_scalogram./10.0));
-colormap rev_magma;
-axis off;
-drawnow();
-
-export_fig(['taslp18_dog-bark_', 'original.png']);
+    export_fig([audio_path, '_original.png']);
 
 %% Iterated reconstruction
 iteration = 1;
