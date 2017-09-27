07d7de20dfe6decdc648250339dafd181803a05f
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-09-27 15:31:53 -0400

    target_scalogram = display_scalogram(target_U1);

--- research/taslp18/taslp18_reconstructions.m
+++ research/taslp18/taslp18_reconstructions.m
@@ -23,11 +23,11 @@ for audio_name_id = 1:length(audio_names)
     % Compute scattering transform.
     target_U0 = initialize_U(target_waveform, vis_archs{1}.banks{1});
     target_Y1 = U_to_Y(target_U0, vis_archs{1}.banks);
-    target_U1 = Y_to_U(target_Y1{end}, vis_arch{1}.nonlinearity);
+    target_U1 = Y_to_U(target_Y1{end}, vis_archs{1}.nonlinearity);
 
     % Display original scalogram.
-    target_scalogram = display_scalogram(target_U{1+1});
-    imagesc(log1p(target_scalogram./100.0));
+    target_scalogram = display_scalogram(target_U1);
+    imagesc(log1p(target_scalogram));
     colormap rev_magma;
     axis off;
     drawnow();
