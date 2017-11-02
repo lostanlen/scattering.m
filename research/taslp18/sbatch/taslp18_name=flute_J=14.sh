069d366cd7c243ed6f9796a20e71d301b20ca116
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-02 15:43:19 -0400

    generate shell scripts

---
+++ research/taslp18/sbatch/taslp18_name=flute_J=14.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing reconstructions.
+
+sbatch taslp18_name=flute_sc=none_J=14_wav=morlet.sbatch
+sbatch taslp18_name=flute_sc=none_J=14_wav=gammatone.sbatch
+sbatch taslp18_name=flute_sc=time_J=14_wav=morlet.sbatch
+sbatch taslp18_name=flute_sc=time_J=14_wav=gammatone.sbatch
+sbatch taslp18_name=flute_sc=time-frequency_J=14_wav=morlet.sbatch
+sbatch taslp18_name=flute_sc=time-frequency_J=14_wav=gammatone.sbatch
