41b83cb955d54da30568d3695e92b0c5b884af29
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-11-02 13:55:45 -0400

    up

---
+++ research/taslp18/generate_taslp18_sbatch.py
@@ -0,0 +1,66 @@
+import os
+import sys
+
+audio_name_id = 1
+modulations_id = 1
+T_id = 1
+wavelet_id = 1
+audio_names = {'taslp18_dog-bark', 'taslp18_flute'};
+modulations_strs = {'none', 'time', 'time-frequency'};
+wavelet_strs = {'morlet', 'gammatone'};
+Js = [11, 14, 17];
+
+modulations_str = modulations_strs{modulations_id};
+T = Ts(T_id);
+wavelet_str = wavelet_strs{wavelet_id};
+
+os.makedirs("sbatch", exist_ok=True)
+os.makedirs("slurm", exist_ok=True)
+
+# Loop over recording units.
+for audio_name_str in audio_namess:
+
+    # Loop over modulations.
+    for modulations_str in modulations_strs:
+
+        # Loop over Js.
+        for J in Js:
+
+            J_str = str(J)
+
+            # Loop over wavelets.
+            for wavelet_str in wavelet_str:
+
+                # Define file path.
+                job_name = "_".join([
+                    "taslp18",
+                    audio_name_str,
+                    modulations_str,
+                    J_str,
+                    wavelet_str
+                ])
+                file_name = job_name + ".sbatch"
+                file_path = os.path.join("sbatch", file_name)
+
+
+                # Open file.
+                with open(file_path, "w") as f:
+                    f.write("#!/bin/bash\n")
+                    f.write("\n")
+                    f.write("#BATCH --job-name=" + job_name + "\n")
+                    f.write("#SBATCH --nodes=1\n")
+                    f.write("#SBATCH --tasks-per-node=1\n")
+                    f.write("#SBATCH --cpus-per-task=1\n")
+                    f.write("#SBATCH --time=0:30:00\n")
+                    f.write("#SBATCH --mem=32GB\n")
+                    f.write("#SBATCH --output=../slurm/slurm_" + job_name + "_%j.out\n")
+                    f.write("\n")
+                    f.write("module purge\n")
+                    f.write("module load/matlab2017a\n")
+                    f.write("\n")s
+                    f.write("matlab -nosplash -nodesktop -nodisplay -r " +
+                        "\"audio_name = " + audio_name_str + "; " +
+                        "modulations_str = " + modulations_str + "; " +
+                        "J = " + J_str + "; " +
+                        "wavelet_str = " + wavelet_str + "; "
+                        "run('../taslp18_reconstructions.m');\"")
