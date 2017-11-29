77bb2d7c87384e2d4ffbd6f60d5b763898551bad
Vincent <vincent.lostanlen@nyu.edu>
2017-11-29 17:49:24 -0500

    up

---
+++ research/taslp18/sbatch/taslp18_name=speech_J=16.sh
@@ -0,0 +1,8 @@
+# This shell script executes the Slurm jobs for computing reconstructions.
+
+sbatch taslp18_name=speech_sc=none_J=16_wav=gammatone.sbatch
+sbatch taslp18_name=speech_sc=none_J=16_wav=morlet.sbatch
+sbatch taslp18_name=speech_sc=time_J=16_wav=gammatone.sbatch
+sbatch taslp18_name=speech_sc=time_J=16_wav=morlet.sbatch
+sbatch taslp18_name=speech_sc=time-frequency_J=16_wav=gammatone.sbatch
+sbatch taslp18_name=speech_sc=time-frequency_J=16_wav=morlet.sbatch
