6dcc39e8d5b1cd223199ee946b8a00af8ecd96e5
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-18 16:19:31 -0500

    write taslp18_mcdermott.sh

---
+++ research/taslp18/sbatch/taslp18_mcdermott.sh
@@ -0,0 +1,5 @@
+# This shell script executes the Slurm jobs for computing reconstructions.
+
+sbatch taslp18_name=dog-bark_mcdermott.sbatch
+sbatch taslp18_name=flute_mcdermott.sbatch
+sbatch taslp18_name=speech_mcdermott.sbatch
