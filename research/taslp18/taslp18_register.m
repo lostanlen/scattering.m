131d7e1caad8394eb045e8e5db2cb9cf8dcacc91
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-18 15:20:15 -0500

    up

--- research/taslp18/taslp18_register.m
+++ research/taslp18/taslp18_register.m
@@ -11,6 +11,7 @@ vis_archs = taslp18_setup_visualization(24, N);
 
 for audio_name_str = audio_names
 
+audio_name_str = audio_name_str{1};
 audio_path = ['media/', audio_name_str, '.wav'];
 disp(audio_path);
 
@@ -19,13 +20,16 @@ target_U0 = initialize_U(target_waveform, vis_archs{1}.banks{1});
 target_Y1 = U_to_Y(target_U0, vis_archs{1}.banks);
 target_U1 = Y_to_U(target_Y1{end}, vis_archs{1}.nonlinearity);
 target_scalogram = display_scalogram(target_U1);
-spectral_flux = sum(abs(diff(log1p(target_scalogram), 1, 2), 1));
-[~, target_argmax] = max(spectral_flux);
+[~, target_argmax] = max(sum(abs(diff(log1p(target_scalogram), 1, 2)), 1));
 
 
 for J = Js
 for scattering_str = scattering_strs
+scattering_str = scattering_str{1};
+
 for wavelet_str = wavelets
+
+wavelet_str = wavelet_str{1};
 
 % Load waveform.
 audio_path = [ ...
@@ -52,7 +56,7 @@ U1 = Y_to_U(Y1{end}, vis_archs{1}.nonlinearity);
 
 % Display original scalogram.
 scalogram = display_scalogram(U1);
-[~, argmax] = max(sum(abs(diff(log1p(scalogram), 1, 2), 1)));
+[~, argmax] = max(sum(abs(diff(log1p(scalogram), 1, 2)), 1));
 registered_scalogram = circshift(log1p(scalogram), ...
     (target_argmax - argmax), 2);
 
@@ -62,6 +66,11 @@ axis off;
 drawnow();
 export_fig([ ...
     'media/', ...
+    audio_name_str, ...
+    '_Q=08', ...
+    '_J=', num2str(J), ...
+    '_sc=', scattering_str, ...
+    '_wvlt=', wavelet_str, '/', ...
     audio_name_str, ...
     '_Q=08', ...
     '_J=', num2str(J), ...
