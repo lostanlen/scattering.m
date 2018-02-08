998bae4e50a9b8c8fca99f068399fbe08524c3b3
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-02-08 03:22:20 -0500

    define time-frequence scattering

--- research/dafx18/beta_reversal/beta_reversal.m
+++ research/dafx18/beta_reversal/beta_reversal.m
@@ -8,8 +8,9 @@ chirp_spans = [1, 1, 1, 1, 1, 1, 1, 1]; % in octaves
 tukey_alpha = 0.5;
 n_chirps = length(chirp_locations);
 
-target_waveform = zeros(N, 1);
 
+% Generate original waveform as a combination of chirps.
+target_waveform = zeros(N, 1);
 for chirp_id = 1:n_chirps
     chirp_location = chirp_locations(chirp_id);
     chirp_frequency = chirp_frequencies(chirp_id);
@@ -31,4 +32,34 @@ for chirp_id = 1:n_chirps
         target_waveform(chirp_start:chirp_stop) + chirp;
 end
 
+
+% Export original waveform.
 target_waveform = 0.1 * target_waveform / max(target_waveform);
+audiowrite('beta_reversal_original.wav', target_waveform, sample_rate);
+
+
+% Compute time-frequency scattering.
+Q = 12;
+T = inter_onset_interval
+opts{1}.time.nFilters_per_octave = Q1;
+opts{1}.time.T = T;
+opts{1}.time.size = N;
+opts{1}.time.is_chunked = false;
+opts{1}.time.duality = 'hermitian';
+opts{1}.time.gamma_bounds = [1+Q1*1 Q1*6];
+opts{1}.time.wavelet_handle = @morlet_1d;
+opts{2}.invariants.time.T = T;
+opts{2}.invariants.time.size = N;
+opts{2}.invariants.time.subscripts = [1];
+opts{2}.invariants.time.duality = 'hermitian';
+opts{2}.banks.time.nFilters_per_octave = 1;
+opts{2}.banks.time.wavelet_handle = @morlet_1d;
+opts{2}.banks.time.duality = 'hermitian';
+opts{2}.banks.time.T = T;
+opts{2}.banks.time.wavelet_handle = @morlet_1d;
+opts{2}.banks.gamma.xi = 1/3;
+opts{2}.banks.gamma.T = 2^3;
+opts{2}.banks.gamma.duality = 'hermitian';
+opts{2}.banks.gamma.nFilters_per_octave = 1;
+opts{2}.banks.gamma.subscripts = 2;
+archs = sc_setup(opts);
