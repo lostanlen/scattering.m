2d7e594c216c1272b6e88e7007cb226f483f9c7d
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2021-04-24 00:22:06 +0200

    update sc_norm

--- lib/export/sc_norm.m
+++ lib/export/sc_norm.m
@@ -8,11 +8,14 @@ if nargin<2
 end
 
 [formatted_S, formatted_layers] = sc_format(S, spatial_subscripts, layers);
+nSamples = size(formatted_S, 2);
 
 layer_norms = zeros(1, nLayers);
-for layer_id = 1:nLayers
-    layer_norms(layer_id) = sum(abs(formatted_layers{layer_id}(:))) * ...
-        2^(1-layer_id);
+layer_norms(1+0) = 0.5 * norm(S{1+0}.data);% / sqrt(nSamples);
+for layer_id = 2:nLayers
+    l2_norm = sqrt(sum(formatted_layers{layer_id}.^2, 2)) / sqrt(nSamples);
+    %l2_norm = sum(formatted_layers{layer_id}, 2) / sqrt(nSamples);
+    layer_norms(layer_id) = sum(l2_norm) * 2^(1-layer_id);
 end
 
 total_norm = sum(layer_norms);
